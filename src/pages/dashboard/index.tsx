81a14f0bea18e3d1a837e1e17dd22a246d4a8431
faheem4531 <faheem4531@gmail.com>
2023-11-07 16:03:04 +0500

    Dashboard page structure

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "@/styles/Dashboard.module.css"
+import NavBar from '@/components/dashboardComponent/Navbar'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  )
+}
+
+export default Dashboard
