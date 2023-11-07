07e0e5614ee0ac88f4ae0525d29a16f049eb6100
usamamalik909 <malik.usama9100@gmail.com>
2023-11-07 19:41:46 +0500

    getTitle

--- src/pages/change-password/getStarted.tsx
+++ src/pages/change-password/getStarted.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Image from "next/image";
 import Arrow from "../../../public/startArrow.png";
 import { Box, Typography } from "@mui/material";
-
 const getStarted = () => {
   return (
     <div>
@@ -20,18 +19,55 @@ const getStarted = () => {
           gap: 0,
         }}
       >
-        <Box sx={{ marginTop: "200px", marginLeft: "150px" }}>
-          <Typography sx={{ fontSize: "60px", color: "white" }}>
+        <Box
+          sx={{
+            paddingTop: { xs: "170px", sm: "140px", md: "120px", lg: "100px", xl:'70px'},
+            paddingLeft: { xs: "20px", sm: "75px", md: "100px", lg: "125px" },
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: { xs: "30px", sm: "40px", md: "50px", lg: "60px" ,xl:'100px'},
+              color: "white",
+            }}
+          >
             Hi Naseer 👋
           </Typography>
         </Box>
-        <Box sx={{ marginLeft: "245px", marginTop: "230px" }}>
-          <Image src={Arrow} alt="arrow" />
+        <Box
+          sx={{
+            paddingLeft: { xs: "140px", sm: "50px", md: "90px", lg: "250px",xl:'800px' },
+            paddingTop: { xs: "140px", sm: "120px", md: "100px", lg: "100px",xl:'100px' }
+            , color:'white'
+          }}
+        >
+          <Image 
+          src={Arrow}
+           alt="arrow" 
+           width={430} 
+           height={100} />
         </Box>
-        <Box sx={{ marginLeft: "235px" }}>
-          <Typography sx={{ fontSize: "150px", color: "white" }}>
-            Let Us Help <span style={{ color: "#197065" }}>You...</span>
+        <Box
+          sx={{
+            display: "flex",
+            gap : {xs : "0px", md: "0px", lg : "0px"},
+            paddingTop: { xs: "140px", sm: "120px", md: "100px", lg: "70px",xl:'150px' },
+            paddingLeft: { xs: "130px", sm: "160px", md: "240px", lg: "300px" ,xl:'920px'}
+        
+          }}
+        
+        >
+          <Typography
+            sx={{
+              fontSize: { xs: "30px", sm: "60px", md: "70px", lg: "90px" ,xl:'100px'},
+              color: "white",
+            }}
+          >
+            Let Us Help{" "}
           </Typography>
+            <Typography sx ={{fontSize: { xs: "35px", sm: "60px", md: "70px", lg: "100px",xl:'100px' }, color: "#197065", paddingLeft: "30px" }}>
+              You...
+            </Typography>
         </Box>
       </Box>
     </div>
