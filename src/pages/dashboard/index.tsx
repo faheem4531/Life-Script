46d36b19505adcfb0dc0b3c29f4f4730ccb4bc24
faheem4531 <faheem4531@gmail.com>
2023-11-08 20:57:44 +0500

    scroll issue for dashboard template

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -7,12 +7,12 @@ import { Box } from "@mui/material";
 
 const Dashboard = () => {
   return (
-    <Box sx={{ backgroundColor: "#FFF9F0", overflowX: "hidden", position: "relative" }}>
+    <Box sx={{ backgroundColor: "#FFF9F0", overflowX: "hidden" }}>
       <Box sx={{ position: "fixed", right: "0", left: "0", top: "0", zIndex: "2" }}>
         <NavBar />
       </Box>
       <Box sx={{ marginTop: "1px", display: "flex", mt: "70px" }}>
-        <Box sx={{ width: "220px", backgroundColor: "#197065", position: "fixed", bottom: "0", top: "70px" }}>
+        <Box sx={{ width: "220px", backgroundColor: "#197065", position: "fixed", bottom: "0", top: "70px", zIndex: "2" }}>
           <SideBar />
         </Box>
         <Box
