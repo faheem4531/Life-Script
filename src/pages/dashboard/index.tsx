a0e75182f2d6b39619e2712e740d2ecf0974f81f
faheem4531 <faheem4531@gmail.com>
2023-11-08 20:47:34 +0500

    main dashboard page sidebar/navbar fixed

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -7,10 +7,12 @@ import { Box } from "@mui/material";
 
 const Dashboard = () => {
   return (
-    <Box sx={{ backgroundColor: "#FFF9F0", overflowX: "hidden" }}>
-      <NavBar />
-      <Box sx={{ marginTop: "1px", display: "flex" }}>
-        <Box sx={{ width: "220px", backgroundColor: "#197065" }}>
+    <Box sx={{ backgroundColor: "#FFF9F0", overflowX: "hidden", position: "relative" }}>
+      <Box sx={{ position: "fixed", right: "0", left: "0", top: "0", zIndex: "2" }}>
+        <NavBar />
+      </Box>
+      <Box sx={{ marginTop: "1px", display: "flex", mt: "70px" }}>
+        <Box sx={{ width: "220px", backgroundColor: "#197065", position: "fixed", bottom: "0", top: "70px" }}>
           <SideBar />
         </Box>
         <Box
@@ -19,6 +21,7 @@ const Dashboard = () => {
             maxWidth: "1600px",
             height: "100%",
             padding: "36px 33px 100px",
+            marginLeft: "220px"
           }}
           className={styles.subContainer}
         >
