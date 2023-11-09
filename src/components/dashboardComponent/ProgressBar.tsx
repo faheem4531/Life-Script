9b4701e2721d2ae5f49eec820a52fc6ba3ca8cdb
usamamalik909 <malik.usama9100@gmail.com>
2023-11-10 04:59:34 +0500

    modifications

--- src/components/dashboardComponent/ProgressBar.tsx
+++ src/components/dashboardComponent/ProgressBar.tsx
@@ -4,26 +4,30 @@ import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 
-const steps = [
-  '20%',
-  '40%',
-  '60%',
-  '80%',
-  '100%'
-];
+const steps = ['20%', '40%', '60%', '80%', '100%'];
 
 export default function ProgressBar() {
+  const activeStep =2; // Set the active step index (0-indexed)
+
   return (
     <Box sx={{ width: '100%' }}>
-      <Stepper activeStep={1}   alternativeLabel sx={{ width:'100%',
-        "& .MuiStepConnector-line": {
+      <Stepper
+        activeStep={activeStep}
+        alternativeLabel
+        sx={{
+          width: '100%',
+          "& .MuiStepConnector-line": {
             border: 'none',
             width: '100%',
             height: '10px',
-            background: '#EDEDED',
-            
-        }
-      }}>
+            background: (theme) => (activeStep === 2 ? '#186F65' : '#EDEDED'), 
+            mt:-0.7
+          },
+          "& .MuiSvgIcon-root": {
+            color: '#186F65',
+          },
+        }}
+      >
         {steps.map((label) => (
           <Step key={label}>
             <StepLabel>{label}</StepLabel>
