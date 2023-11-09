cea29d8f4d822e4dc053afc4f7f88bb37b17ced2
faheem4531 <faheem4531@gmail.com>
2023-11-09 20:42:07 +0500

    change page routs

--- src/pages/dashboard/chapters/index.tsx
+++ src/pages/dashboard/chapters/index.tsx
@@ -8,22 +8,11 @@ import { Box } from "@mui/material";
 const Dashboard = () => {
   return (
     <Box sx={{ backgroundColor: "#FFF9F0", overflowX: "hidden" }}>
-      <Box
-        sx={{ position: "fixed", right: "0", left: "0", top: "0", zIndex: "2" }}
-      >
+      <Box sx={{ position: "fixed", right: "0", left: "0", top: "0", zIndex: "2" }}>
         <NavBar />
       </Box>
       <Box sx={{ marginTop: "1px", display: "flex", mt: "70px" }}>
-        <Box
-          sx={{
-            width: "220px",
-            backgroundColor: "#197065",
-            position: "fixed",
-            bottom: "0",
-            top: "70px",
-            zIndex: "2",
-          }}
-        >
+        <Box sx={{ width: "220px", backgroundColor: "#197065", position: "fixed", bottom: "0", top: "70px", zIndex: "2" }}>
           <SideBar />
         </Box>
         <Box
@@ -37,8 +26,7 @@ const Dashboard = () => {
           className={styles.subContainer}
         >
           <HomeSteps />
-          <Box sx={{ marginTop: "48px" }}>
-            className={styles.CardsContainer}
+          <Box sx={{ marginTop: "48px" }} className={styles.CardsContainer}>
             <DetailCard />
             <DetailCard />
             <DetailCard />
