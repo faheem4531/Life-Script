fc770c9c826dc588b2db6723b09364d31189af34
Asif Ali Hanjra <de.asifalihanjra@gmail.com>
2023-11-12 10:58:01 +0500

    build error

--- src/pages/events/modalTest.tsx
+++ src/pages/events/modalTest.tsx
@@ -1,19 +1,6 @@
-import CustomizationDialog from "@/components/modal/CustomizationDialog";
 import { useState } from "react";
-import AddQuestion from "./addQuestion";
 
 export default function Modal() {
   const [openModal, setOpenModal] = useState(false);
-  return (
-    <>
-      <CustomizationDialog
-        open={true}
-        title="Add new question"
-        handleClose={() => {}}
-        customStyles={{ backgroundColor: "auto" }}
-      >
-        <AddQuestion />
-      </CustomizationDialog>
-    </>
-  );
+  return <></>;
 }
