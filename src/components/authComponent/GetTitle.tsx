391abcaa3a352a9ed29244444c8e44be25ec1bd1
faheem4531 <faheem4531@gmail.com>
2023-11-14 17:34:23 +0500

    get title responsiveness and fix issues

--- src/components/authComponent/GetTitle.tsx
+++ src/components/authComponent/GetTitle.tsx
@@ -1,9 +1,11 @@
-import { Box, TextField, Typography } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import Arrow from "../../../public/startArrow.png";
 import React from "react";
 import Image from "next/image";
 import BookImage from "../../../public/getTitleBook.svg";
 import { useState } from "react";
+import styles from "./GetTitle.module.css"
+
 const GetTitle = () => {
   const [text, setText] = useState("");
   const maxLength = 20; // Set the maximum character count to 20
@@ -24,46 +26,53 @@ const GetTitle = () => {
           backgroundRepeat: "no-repeat", // Adjust as needed
           width: "100%",
           height: "100vh", // Adjust the height as needed
-          // overflow: "hidden",
           margin: 0,
           padding: 0,
           gap: 0,
           display: "flex",
           justifyContent: "space-around",
+          color: "#000"
         }}
       >
-        <Box>
+        <Box sx={{ marginLeft: "30px" }}>
           <Typography
             sx={{ fontSize: "53px", fontWeight: "400", marginTop: "153px" }}
+            className={styles.primaryText}
           >
             Hi{" "}
-            <span style={{ fontSize: "60px", fontWeight: "600" }}>Naseer,</span>
+            <span style={{ fontSize: "60px", fontWeight: "600" }} className={styles.boldText}>Naseer,</span>
           </Typography>
           <Typography
             sx={{ fontWeight: "400", fontSize: "53px", marginTop: "32px" }}
+            className={styles.primaryText}
           >
             What would you like to <br /> call your lifescript?
           </Typography>
-          <Box sx={{ marginTop: "150px" }}>
-          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-  <TextField
-    variant="standard"
-    value={text}
-    onChange={handleChange}
-    sx={{ maxWidth: '540px' }}
-    InputProps={{
-      style: { fontSize: '43px' },
-    }}
-  />
-  <Typography sx={{ alignSelf: 'flex-end', fontSize:'30px',color:'#969696' }}>
-    {text.length}/{maxLength}
-  </Typography>
-</Box>
-
+          <Box>
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+              <TextField
+                variant="standard"
+                value={text}
+                onChange={handleChange}
+                sx={{ maxWidth: '540px', minWidth: '120px', marginTop: "30px" }}
+                className={styles.primaryText}
+                InputProps={{
+                  style: { fontSize: '43px' },
+                }}
+              />
+              <Typography sx={{ alignSelf: 'flex-end', fontSize: '30px', color: '#969696' }}>
+                {text.length}/{maxLength}
+              </Typography>
+            </Box>
+            <Button sx={{ width: "200px", padding: "14px 0", bgcolor: "#FCE09B", borderRadius: "30px", color: "#186F65", fontWeight: 600, fontSize: "18px" }}>
+              Start Writing
+            </Button>
           </Box>
         </Box>
-        <Box sx={{ marginTop: "150px",marginRight:'23px' }}>
-          <Image src={BookImage} alt="book image" width={608} height={729} />
+        <Box
+          sx={{ height: "100%", display: "flex", alignItems: "center" }}
+          className={styles.bookDiv}>
+          <Image src={BookImage} alt="book image" className={styles.book} />
         </Box>
       </Box>
     </div>
