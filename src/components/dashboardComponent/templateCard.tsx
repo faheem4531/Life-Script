a52809d7045f8af0cd3fa5c0b486028ff1dafc28
faheem4531 <faheem4531@gmail.com>
2023-11-15 19:29:32 +0500

    fully responsive template page and its components

--- src/components/dashboardComponent/templateCard.tsx
+++ src/components/dashboardComponent/templateCard.tsx
@@ -4,85 +4,86 @@ import { Divider, Typography } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Image from "next/image";
+import styles from "./HomeSteps.module.css"
+
+
 export default function TemplateCard() {
+
   return (
-    <Card sx={{ maxWidth: 500, borderRadius: "6.5px" }}>
-      <CardContent>
+    <Card sx={{ maxWidth: 400, borderRadius: "6.5px" }}>
+      <CardContent sx={{ padding: { xs: "5px 5px 35px 5px" } }}>
         <Typography
           variant="body2"
           color="text "
           fontWeight="600"
-          fontSize="19px"
           textAlign="center"
-          marginTop="10px"
+          sx={{
+            fontSize: { sm: "19px", xs: "12px" },
+            marginTop: { sm: "10px", xs: "0px" }
+          }}
         >
           The Book Of John Doe
         </Typography>
         <Divider
           sx={{
-            width: "245px",
+            width: { sm: "245px", xs: "150px" },
             backgroundColor: "#000",
             height: "2px",
             margin: "9px auto 0",
-            marginBottom: "30px",
+            marginBottom: { sm: "30px", xs: "12px" },
           }}
         />
         <Typography
           display="flex"
-          alignItems="center"
           columnGap="10px"
           color="rgba(22, 22, 22, 0.90)"
-          fontSize="10px"
           marginTop="5px"
+          sx={{ columnGap: { sm: "10px", xs: "3px" }, fontSize: { sm: "10px", xs: "7px" } }}
         >
-          <Image alt="check" src={Tick} />
+          <Image alt="check" src={Tick} className={styles.tick} />
           Winner of the National Academy of Sciences Best Book Award in 2012
         </Typography>
         <Typography
           display="flex"
-          alignItems="center"
           columnGap="10px"
           color="rgba(22, 22, 22, 0.90)"
-          fontSize="10px"
           marginTop="5px"
+          sx={{ columnGap: { sm: "10px", xs: "3px" }, fontSize: { sm: "10px", xs: "7px" } }}
         >
-          <Image alt="check" src={Tick} />
+          <Image alt="check" src={Tick} className={styles.tick} />
           The Worst Job Ever
         </Typography>
         <Typography
           display="flex"
-          alignItems="center"
           columnGap="10px"
           color="rgba(22, 22, 22, 0.90)"
-          fontSize="10px"
           marginTop="5px"
+          sx={{ columnGap: { sm: "10px", xs: "3px" }, fontSize: { sm: "10px", xs: "7px" } }}
         >
-          <Image alt="check" src={Tick} />
+          <Image alt="check" src={Tick} className={styles.tick} />
           The Best Job Ever
         </Typography>
         <Typography
           display="flex"
-          alignItems="center"
           columnGap="10px"
           color="rgba(22, 22, 22, 0.90)"
-          fontSize="10px"
           marginTop="5px"
+          sx={{ columnGap: { sm: "10px", xs: "3px" }, fontSize: { sm: "10px", xs: "7px" } }}
         >
-          <Image alt="check" src={Tick} />
+          <Image alt="check" src={Tick} className={styles.tick} />
           Your Best Employee
         </Typography>
         <Typography
           display="flex"
-          alignItems="center"
           columnGap="10px"
           color="rgba(22, 22, 22, 0.90)"
-          fontSize="10px"
           marginTop="5px"
+          sx={{ columnGap: { sm: "10px", xs: "3px" }, fontSize: { sm: "10px", xs: "7px" } }}
         >
-          <Image alt="check" src={Tick} />
+          <Image alt="check" src={Tick} className={styles.tick} />
           The Best Project You've Worked On
         </Typography>
       </CardContent>
-    </Card>
+    </Card >
   );
 }
--- src/pages/dashboard/templates/index.tsx
+++ src/pages/dashboard/templates/index.tsx
@@ -4,39 +4,23 @@ import SideBar from "@/components/dashboardComponent/Sidebar";
 import ChooseTemplate from "@/components/dashboardComponent/ChooseTemplate";
 import styles from "@/styles/Dashboard.module.css";
 import { Box } from "@mui/material";
+import Layout from "@/components/Layout/Layout";
 
 const Dashboard = () => {
   return (
-    <Box sx={{ backgroundColor: "#FFF9F0", overflowX: "hidden" }}>
-      <Box sx={{ position: "fixed", right: "0", left: "0", top: "0", zIndex: "2" }}>
-        <NavBar />
-      </Box>
-      <Box sx={{ marginTop: "1px", display: "flex", mt: "70px" }}>
-        <Box sx={{ width: "220px", backgroundColor: "#197065", position: "fixed", bottom: "0", top: "70px", zIndex: "2" }}>
-          <SideBar />
-        </Box>
-        <Box
-          sx={{
-            width: "100%",
-            maxWidth: "1600px",
-            height: "100%",
-            padding: "36px 33px 100px",
-            marginLeft: "220px"
-          }}
-          className={styles.subContainer}
-        >
-          <ChooseTemplate />
-          <Box sx={{ marginTop: "48px" }} className={styles.CardsContainer}>
-            <DetailCard />
-            <DetailCard />
-            <DetailCard />
-            <DetailCard />
-            <DetailCard />
-            <DetailCard />
-          </Box>
+    <Layout>
+      <Box className={styles.subContainer}>
+        <ChooseTemplate />
+        <Box sx={{ marginTop: { sm: "48px", xs: "30px" } }} className={styles.CardsContainer}>
+          <DetailCard />
+          <DetailCard />
+          <DetailCard />
+          <DetailCard />
+          <DetailCard />
+          <DetailCard />
         </Box>
       </Box>
-    </Box>
+    </Layout>
   );
 };
 
