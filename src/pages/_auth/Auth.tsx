e2cccb9e12823420433a94d34ab0315579016729
faheem4531 <faheem4531@gmail.com>
2023-11-17 17:34:09 +0500

    pagre revert

--- src/pages/_auth/Auth.tsx
+++ src/pages/_auth/Auth.tsx
@@ -1,17 +1,17 @@
-import GetTitle from "@/components/authComponent/GetTitle";
+import Register from "@/components/authComponent/Register";
 import { Box } from "@mui/material";
 
 export default function AuthPage() {
   return (
     <Box>
-      {/* <Register /> */}
+      <Register />
       {/* <ForgetPassword/> */}
       {/* <ForgetPasswordLink/> */}
       {/* <EmailVerification/> */}
       {/* <EmailVerificationLink/> */}
-      {/* <LetsStarted/> */}
+      {/* <LetsStarted /> */}
       {/* <ResetPassword/> */}
-      <GetTitle />
+      {/* <GetTitle /> */}
     </Box>
   );
 }
