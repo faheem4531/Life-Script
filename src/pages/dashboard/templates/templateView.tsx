49eb87a082552d4cb2df735aca19abc47d119468
usamamalik909 <malik.usama9100@gmail.com>
2023-11-23 18:18:26 +0500

    UI fixes

--- src/pages/dashboard/templates/templateView.tsx
+++ src/pages/dashboard/templates/templateView.tsx
@@ -18,7 +18,7 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import addIcon from "../../../../public/addicon.svg";
+import addIcon from "../../../../public/bookicon.svg";
 const chapterName = () => {
   const [openModal, setOpenModal] = useState(false);
   const [loading, setLoading] = useState(true);
