d27b3ad8d8010655f6ce798a746989a052f6cced
usamamalik909 <malik.usama9100@gmail.com>
2023-11-27 13:51:07 +0500

    Hover Effect

--- src/components/dashboardComponent/CreateChapterCard.tsx
+++ src/components/dashboardComponent/CreateChapterCard.tsx
@@ -3,6 +3,7 @@
 import AddChapterImage from "@/_assets/svg/add-chapter-green.svg";
 import AddChapterWhite from "@/_assets/svg/add-chapter-white.svg";
 import TemplateWhite from "@/_assets/svg/template-white.svg";
+import Template from "@/_assets/svg/template.svg";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import Typography from "@mui/material/Typography";
@@ -16,84 +17,133 @@ interface ChapterProps {
 export const StartNewChapter = ({ addChapterClick }: ChapterProps) => {
   const router = useRouter();
   const [newChapter, setNewChapter] = useState(true);
+  const [hoverStartNewChapter, setHoverStartNewChapter] = useState(false);
+  const [hoverUseTemplate, setHoverUseTemplate] = useState(false);
+
+  const handleHoverStartNewChapter = () => {
+    setHoverStartNewChapter(true);
+  };
+
+  const handleLeaveStartNewChapter = () => {
+    setHoverStartNewChapter(false);
+  };
+
+  const handleHoverUseTemplate = () => {
+    setHoverUseTemplate(true);
+  };
+
+  const handleLeaveUseTemplate = () => {
+    setHoverUseTemplate(false);
+  };
+
+  const handleTemplateClick = () => {
+    router.push("//dashboard/templates");
+  };
   return (
     <Card
-      className="container-fontfamily"
-      sx={{
-        borderRadius: "6.5px",
-        height: "280px",
-        padding: "11px",
-      }}
-    >
-      {newChapter && (
+    className="container-fontfamily"
+    sx={{
+      borderRadius: "6.5px",
+      height: "280px",
+      padding: "11px",
+    }}
+  >
+    {newChapter && (
+      <Box
+        sx={{
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          rowGap: "17px",
+          justifyContent: "center",
+          border: "1.6px dashed #CDCDCD",
+          borderRadius: "4px",
+        }}
+        onClick={() => setNewChapter(false)}
+      >
+        <Image alt="image" src={AddChapterImage} />
+        <Typography
+          sx={{ color: "#197065", fontWeight: 600, fontSize: "14px" }}
+        >
+          Start new chapter
+        </Typography>
+      </Box>
+    )}
+    {!newChapter && (
+      <Box sx={{ height: "100%" }}>
         <Box
           sx={{
-            height: "100%",
+            height: "49%",
             display: "flex",
-            flexDirection: "column",
             alignItems: "center",
-            rowGap: "17px",
+            columnGap: "17px",
             justifyContent: "center",
-            border: " 1.6px dashed #CDCDCD",
+            border: "1.6px dashed #CDCDCD",
             borderRadius: "4px",
+            marginBottom: "2%",
+            cursor: "pointer",
+            "&:hover": {
+              borderColor: "#197065",
+              borderStyle: "dashed",
+              color: "#197065",
+            },
           }}
-          onClick={() => setNewChapter(false)}
+          onMouseEnter={handleHoverStartNewChapter}
+          onMouseLeave={handleLeaveStartNewChapter}
+          onClick={addChapterClick}
         >
-          <Image alt="image" src={AddChapterImage} />
+          <Image
+            alt="image"
+            src={hoverStartNewChapter ? AddChapterImage : AddChapterWhite}
+          />
           <Typography
-            sx={{ color: "#197065", fontWeight: 600, fontSize: "14px" }}
+            sx={{
+              color: hoverStartNewChapter ? "#197065" : "#CDCDCD",
+              fontWeight: 600,
+              fontSize: "14px",
+            }}
           >
             Start new chapter
           </Typography>
         </Box>
-      )}
-      {!newChapter && (
-        <Box sx={{ height: "100%" }}>
-          <Box
+
+        <Box
+          sx={{
+            height: "49%",
+            display: "flex",
+            alignItems: "center",
+            columnGap: "17px",
+            justifyContent: "center",
+            border: "1.6px dashed #CDCDCD",
+            borderRadius: "4px",
+            cursor: "pointer",
+            "&:hover": {
+              borderColor: "#197065",
+              borderStyle: "dashed",
+              color: "#197065",
+            },
+          }}
+          onMouseEnter={handleHoverUseTemplate}
+          onMouseLeave={handleLeaveUseTemplate}
+          onClick={handleTemplateClick}
+        >
+          <Image
+            alt="image"
+            src={hoverUseTemplate ? Template : TemplateWhite}
+          />
+          <Typography
             sx={{
-              height: "49%",
-              display: "flex",
-              alignItems: "center",
-              columnGap: "17px",
-              justifyContent: "center",
-              border: " 1.6px dashed #CDCDCD",
-              borderRadius: "4px",
-              marginBottom: "2%",
-              cursor: "pointer",
-            }}
-            onClick={addChapterClick}
-          >
-            <Image alt="image" src={AddChapterWhite} />
-            <Typography
-              sx={{ color: "#CDCDCD", fontWeight: 600, fontSize: "14px" }}
-            >
-              Start new chapter
-            </Typography>
-          </Box>
-          <Box
-            sx={{
-              height: "49%",
-              display: "flex",
-              alignItems: "center",
-              columnGap: "17px",
-              justifyContent: "center",
-              border: " 1.6px dashed #CDCDCD",
-              borderRadius: "4px",
-              cursor: "pointer",
-            }}
-            onClick={() => {
-              router.push("//dashboard/templates");
+              color: hoverUseTemplate ? "#197065" : "#CDCDCD",
+              fontWeight: 600,
+              fontSize: "14px",
             }}
           >
-            <Image alt="image" src={TemplateWhite} />
-            <Typography
-              sx={{ color: "#CDCDCD", fontWeight: 600, fontSize: "14px" }}
-            >
-              Use template
-            </Typography>
-          </Box>
+            Use template
+          </Typography>
         </Box>
-      )}
-    </Card>
+      </Box>
+    )}
+  </Card>
   );
 };
