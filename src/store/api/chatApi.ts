ae75d21b2bc673e41e0ff2df39ecbf21638255fe
Asif Ali Hanjra <de.asifalihanjra@gmail.com>
2023-11-28 16:09:38 +0500

    audio api's

--- src/store/api/chatApi.ts
+++ src/store/api/chatApi.ts
@@ -58,6 +58,20 @@ export async function uploadImageApi(data) {
   }
 }
 
+export async function uploadAudioApi(data) {
+  try {
+    const res = await api.post("/questions/speech-to-text", data);
+    return res;
+  } catch (error: any) {
+    if (typeof error?.response?.data?.message === "object") {
+      const errors = error?.response?.data?.message?.message;
+      throw new Error(errors ? errors[0] : "Failed");
+    } else {
+      throw new Error(error.response?.data?.message);
+    }
+  }
+}
+
 export async function saveAnswerApi(data: any) {
   try {
     const res = await api.post("/users/save-answer", data);
