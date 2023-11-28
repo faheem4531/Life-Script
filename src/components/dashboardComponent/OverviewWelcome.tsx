fe9246b404c6eaa958ffa31edfbecce048c56465
faheem4531 <faheem4531@gmail.com>
2023-11-28 21:01:08 +0500

    overview screen responsiveness fixes

--- src/components/dashboardComponent/OverviewWelcome.tsx
+++ src/components/dashboardComponent/OverviewWelcome.tsx
@@ -5,26 +5,40 @@ import { Box, Typography } from '@mui/material'
 import Welcome from "@/_assets/png/overview-welcome-card.png"
 import Button from '../button/Button'
 import ContineWriting from "@/_assets/svg/continue-writing.svg"
+import BgRounded from "@/_assets/png/overview-welcome-bg.png"
 
 const WelcomeOverview = () => {
   return (
     <Box sx={{
       backgroundColor: "#197065",
       color: "#fff",
-      borderRadius: { sm: "24px", xs: "5px" },
-      padding: { sm: "30px 50px", xs: "17px 30px" },
+      borderRadius: "14px",
+      padding: { xl: "30px 35px", lg: "25px 30px", xs: "17px 30px" },
       display: "flex",
       alignItems: "center",
       justifyContent: "space-between",
       maxWidth: "1149px",
-      width: "100%"
+      width: "100%",
+      maxHeight: "230px",
+      minWidth: "700px",
+      position: "relative"
     }}
       className={styles.welcomeMain}
     >
-      <Box>
-        <Typography sx={{ fontSize: "25px" }}> Welcome back ...</Typography>
-        <Typography sx={{ fontSize: "45px", fontWeight: 700 }}> Haseeb!</Typography>
-        <Typography sx={{ fontSize: "19px", fontWeight: 300, lineHeight: "150%", marginBottom: "20px", color: "#a8c9c5" }}>
+      <Box >
+        <Typography sx={{ fontSize: { xl: "18px", sm: "20px" } }}> Welcome back ...</Typography>
+        <Typography sx={{
+          fontSize: { xl: "33px", sm: "35px" },
+          lineHeight: "120%",
+          fontWeight: 700
+        }}> Haseeb!</Typography>
+        <Typography sx={{
+          fontSize: { xl: "14px", sm: "17px" },
+          fontWeight: 300,
+          lineHeight: "150%",
+          marginBottom: "20px",
+          color: "#a8c9c5"
+        }}>
           “Success is not final, failure is not fatal: It is the courage to continue that counts.”
         </Typography>
         <Button
@@ -33,12 +47,15 @@ const WelcomeOverview = () => {
           background="#fff"
           borderRadius="23px"
           color="#186F65"
+          width="180px"
+          fontSize="14px"
           padding="10px 18px"
           onClick={() => { }}
         />
       </Box>
 
       <Image alt='Welcome' src={Welcome} className={styles.welcomeImage} />
+      <Image alt='Welcome' src={BgRounded} className={styles.bgRounded} />
     </Box >
   )
 }
