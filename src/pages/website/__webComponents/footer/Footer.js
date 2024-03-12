e787aca921a7c6d35fb3aeebd389a6449edea60b
faheem4531 <faheem4531@gmail.com>
2024-03-12 15:59:17 +0500

    footer

--- src/pages/website/__webComponents/footer/Footer.js
+++ src/pages/website/__webComponents/footer/Footer.js
@@ -12,19 +12,20 @@ const Footer = () => {
     <Box>
 
       <Box sx={{
-        padding: "44px 55px 40px 100px",
+        padding: { lg: "44px 55px 40px 100px", sm: "44px 30px 40px 70px", xs: "40px 0" },
         bgcolor: "#30422E",
         color: "#F3ECDA",
         display: 'flex',
-        justifyContent: "space-between"
+        justifyContent: { sm: "space-between", xs: "center" }
       }}>
         <Box>
           <Image src={Logo} alt="logo" />
-          <Typography sx={{ width: "210px", fontSize: "16px", margin: "23px 0" }}>Echoes of the past, blueprint for the future.</Typography>
+          <Typography sx={{ width: "210px", fontSize: "16px", margin: "23px 0", textAlign: { xs: "center", sm: "start" } }}>Echoes of the past, blueprint for the future.</Typography>
           <Box sx={{
             display: "flex",
             columnGap: "13px",
-            alignItems: "center"
+            alignItems: "center",
+            justifyContent: { xs: "center", sm: "start" }
           }}>
             <Image src={Insta} alt="logo" />
             <Image src={Li} alt="logo" />
@@ -34,19 +35,19 @@ const Footer = () => {
         </Box>
 
         <Box sx={{ display: "flex", columnGap: "45px" }}>
-          <Box>
+          <Box sx={{ display: { sm: "block", xs: "none" } }}>
             <Typography sx={{ fontWeight: 800, marginBottom: "19px" }}>Company</Typography>
             <Typography sx={{ fontSize: "16px", marginBottom: "19px" }}>About</Typography>
             <Typography sx={{ fontSize: "16px" }}>Features</Typography>
           </Box>
-          <Box>
+          <Box sx={{ display: { sm: "block", xs: "none" } }}>
             <Typography sx={{ fontWeight: 800, marginBottom: "19px" }}>Help</Typography>
             <Typography sx={{ fontSize: "16px", marginBottom: "19px" }}>Customer Suport</Typography>
             <Typography sx={{ fontSize: "16px", marginBottom: "19px" }}>Delivery Details</Typography>
             <Typography sx={{ fontSize: "16px", marginBottom: "19px" }}>Terms & Conditions</Typography>
             <Typography sx={{ fontSize: "16px" }}>Privacy Policy</Typography>
           </Box>
-          <Box>
+          <Box sx={{ display: { md: "block", sm: "none", xs: "none" } }}>
             <Typography sx={{ fontWeight: 800, marginBottom: "19px" }}>Resources</Typography>
             <Typography sx={{ fontSize: "16px", marginBottom: "19px" }}>Free ebooks</Typography>
             <Typography sx={{ fontSize: "16px", marginBottom: "19px" }}>Development Tutorial</Typography>
@@ -56,7 +57,7 @@ const Footer = () => {
         </Box>
       </Box >
 
-      <Box sx={{ textAlign: "center", padding: "23px 0" }}>© Copyright  2024 LifeScript</Box>
+      <Box sx={{ textAlign: "center", padding: "23px 0", fontSize: { xs: "12px", sx: "14px" } }}>© Copyright  2024 LifeScript</Box>
     </Box>
   )
 }
