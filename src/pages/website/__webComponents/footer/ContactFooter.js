2171e964f8f2cf0fa961c879986c20cb4d363151
faheem4531 <faheem4531@gmail.com>
2024-03-12 15:49:39 +0500

    contact footer

--- src/pages/website/__webComponents/footer/ContactFooter.js
+++ src/pages/website/__webComponents/footer/ContactFooter.js
@@ -8,32 +8,55 @@ import Input from "../input/Input"
 const ContactFooter = ({ title, subTitle, input1, input2, input3, button, shape }) => {
   return (
     <Box
-      sx={{ padding: "70px 0 50px", display: "flex", flexDirection: "column", alignItems: "center", color: "#F3ECDA" }}
+      sx={{
+        padding: { md: "70px 40px 50px", sm: "70px 30px 50px", xs: "40px 20px" },
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        color: "#F3ECDA"
+      }}
       className={styles.contactFooterBox}
     >
-      <Typography sx={{ fontSize: "54px", fontWeight: 500, fontFamily: "Besley !important" }}>{title}</Typography>
-      <Typography sx={{ fontSize: "16px", fontWeight: 500, marginTop: "30px" }}>{subTitle}</Typography>
+      <Typography sx={{ fontSize: { md: "54px", sm: "44px", xs: "32px" }, textAlign: "center", fontWeight: 500, fontFamily: "Besley !important" }}>{title}</Typography>
+      <Typography sx={{ fontSize: "16px", textAlign: "center", fontWeight: 500, marginTop: "30px" }}>{subTitle}</Typography>
 
-      <Box sx={{ display: "flex", columnGap: "8px", margin: "55px 0 40px", width: "100%", justifyContent: "center" }}>
-        <Input
-          placeHolder={input1}
-        />
-        <Input
-          placeHolder={input2}
-        />
-        <Input
-          placeHolder={input3}
-          img={shape}
-        />
+      <Box sx={{
+        display: "flex",
+        columnGap: "8px",
+        rowGap: "20px",
+        flexDirection: { sm: "row", xs: "column" },
+        margin: { sm: "55px 0 40px", xs: "35px 0 20px" },
+        width: "100%",
+        justifyContent: "center"
+      }}>
+
+        <Box sx={{ width: { sm: "375px", xs: "100%" } }}>
+          <Input
+            placeHolder={input1}
+          />
+        </Box>
+        <Box sx={{ width: { sm: "375px", xs: "100%" } }}>
+          <Input
+            placeHolder={input2}
+          />
+        </Box>
+        <Box sx={{ width: { sm: "375px", xs: "100%" } }}>
+          <Input
+            placeHolder={input3}
+            img={shape}
+          />
+        </Box>
       </Box>
 
-      <Button
-        title={button}
-        width='200px'
-        height='50px'
-        backgroundColor="#30422E"
-        img2={Smily}
-      />
+      <Box sx={{ width: { sm: "200px", xs: "100%" } }}>
+        <Button
+          title={button}
+          width='100%'
+          height='50px'
+          backgroundColor="#30422E"
+          img2={Smily}
+        />
+      </Box>
     </Box>
   )
 }
