fa076aa2fc57b1dd8e4a13a34e3cc72fe468fc51
faheem4531 <faheem4531@gmail.com>
2024-03-13 07:15:01 +0500

    hero => pricing

--- src/pages/website/gifting/sections/Introduction.js
+++ src/pages/website/gifting/sections/Introduction.js
@@ -16,21 +16,24 @@ const HeroGifting = () => {
       justifyContent: "center",
       alignItems: "center",
       color: "#F4F4F4",
-      textAlign: "center"
+      textAlign: "center",
+      padding: { sm: "0", xs: "0 20px" }
     }}
       className={styles.giftIntro}
     >
       <Image src={Logo} alt="logo" className={styles.logo} />
       <PrimaryHeading showStyle={false} heading="The Commemoration Chronicle" color="#F3ECDA" />
-      <Typography sx={{ fontSize: "16px", maxWidth: "720px", margin: "40px 0 30px" }}>
+      <Typography sx={{ fontSize: "16px", maxWidth: "720px", margin: { sm: "40px 0 30px", xs: "25px 0 30px" } }}>
         Craft an ideal gift: a compilation of stories shared by friends and family, elegantly gathered within a stunning book.
       </Typography>
-      <Button
-        title='Start Writing for Free'
-        width='230px'
-        height='55px'
-        img1={Pen}
-      />
+      <Box sx={{ width: { sm: "230px", xs: "100%" } }}>
+        <Button
+          title='Start Writing for Free'
+          width='100%'
+          height='55px'
+          img1={Pen}
+        />
+      </Box>
       <Typography
         sx={{ fontSize: '11px', lineHeight: '24px', fontWeight: 500, margin: '10px 0 150px' }}
       >No credit card required
