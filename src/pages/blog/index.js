1c91132d448747700b5b8d104e276efb70baf2b5
faheem4531 <faheem4531@gmail.com>
2024-03-21 14:02:50 +0500

    blog meta data

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,5 +1,6 @@
 'use client'
 
+import Head from 'next/head';
 import { Box } from "@mui/material";
 import Footer from "@/__webComponents/footer/Footer";
 import ContactFooter from "@/__webComponents/footer/ContactFooter";
@@ -8,21 +9,33 @@ import Blogs from "./sections/Blog";
 
 const BlogPage = () => {
   return (
-    <Box sx={{ bgcolor: "#f3ecda", color: "#3e4f3c" }}>
-      <IntroductionBlog />
-      <Blogs />
-      <ContactFooter
-        title="Still confused? Ask"
-        marked=" away!"
-        lineWidth={150}
-        subTitle="Contact us Now!!!"
-        input1="Your name"
-        input2="Your email address"
-        input3="What’s on your mind?"
-        button="Get in touch!"
-      />
-      <Footer />
-    </Box>
+
+    <>
+      <Head>
+        <title>Blog - Insights, Inspiration, and Practical Guides to Storytelling</title>
+        <meta
+          name="description"
+          content="Your Guide to Capturing Life's Moments, Unlocking the Secrets of Personal Narratives and Leaving a Legacy Like No Other."
+        />
+      </Head>
+
+
+      <Box sx={{ bgcolor: "#f3ecda", color: "#3e4f3c" }}>
+        <IntroductionBlog />
+        <Blogs />
+        <ContactFooter
+          title="Still confused? Ask"
+          marked=" away!"
+          lineWidth={150}
+          subTitle="Contact us Now!!!"
+          input1="Your name"
+          input2="Your email address"
+          input3="What’s on your mind?"
+          button="Get in touch!"
+        />
+        <Footer />
+      </Box>
+    </>
   )
 }
 
