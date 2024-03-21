65219ad7fe7e80ee996961ae56d1a2a0a07a565f
faheem4531 <faheem4531@gmail.com>
2024-03-21 14:06:01 +0500

    faqs meta data

--- src/pages/faqs/index.js
+++ src/pages/faqs/index.js
@@ -1,5 +1,6 @@
 'use client'
 
+import Head from "next/head";
 import { Box } from "@mui/material";
 import Footer from "@/__webComponents/footer/Footer";
 import ContactFooter from "@/__webComponents/footer/ContactFooter";
@@ -12,19 +13,29 @@ import Logo from "@/__webAssets/svgs/logo-footer.svg"
 const FaqPage = () => {
 
   return (
-    <Box sx={{ minHeight: "100vh", bgcolor: "#f3ecda", color: "#3e4f3c" }} className={styles.faqPage}>
-      <NavBar color="#F3ECDA" logo={Logo} />
-      <Faqs />
-      <ContactFooter
-        title="Still have any questions?"
-        subTitle="Contact us Now!!!"
-        input1="Your name"
-        input2="Your email address"
-        input3="What’s on your mind?"
-        button="Get in touch!"
-      />
-      <Footer />
-    </Box>
+    <>
+      <Head>
+        <title>Frequently Asked Questions </title>
+        <meta
+          name="description"
+          content="Want to know more about Lifescript? Here, you’ll find answers to the questions we get asked the most about."
+        />
+      </Head>
+
+      <Box sx={{ minHeight: "100vh", bgcolor: "#f3ecda", color: "#3e4f3c" }} className={styles.faqPage}>
+        <NavBar color="#F3ECDA" logo={Logo} />
+        <Faqs />
+        <ContactFooter
+          title="Still have any questions?"
+          subTitle="Contact us Now!!!"
+          input1="Your name"
+          input2="Your email address"
+          input3="What’s on your mind?"
+          button="Get in touch!"
+        />
+        <Footer />
+      </Box>
+    </>
   )
 }
 
