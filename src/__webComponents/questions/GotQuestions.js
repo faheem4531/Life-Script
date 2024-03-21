f1b59c71954e998a029f688d936d61e6de72b156
faheem4531 <faheem4531@gmail.com>
2024-03-21 15:45:36 +0500

    Populate the questions with answers

--- src/__webComponents/questions/GotQuestions.js
+++ src/__webComponents/questions/GotQuestions.js
@@ -12,51 +12,50 @@ const GotQuestions = () => {
   const [DumyQs1, setDummyQ] = useState([
     {
       qs: "What is included in each subscription?",
-      ans: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lore  ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus exsit amet blandit leo lobortis eget.",
+      ans: "Full year of access to our platform, full-color hardcover printed book and free delivery.",
       panel: "panel1",
       isexpanded: false,
     },
     {
       qs: "Can I gift a subscription and how does that work?",
-      ans: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lore  ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus exsit amet blandit leo lobortis eget.",
+      ans: "Absolutely, gifting a subscription is an easy, thoughtful and one of the best last-minute presents. You simply provide the email of the fortunate recipient and choose the date when you’d like the gift card to be sent. Once your purchase is complete, you’ll receive a confirmation email with a link to print out a beautiful gift card. You have the flexibility to email it right away, schedule a time when you want us to send it or print it out for a more personal touch.",
       panel: "panel2",
       isexpanded: false,
-
     },
     {
       qs: "Is Lifescript available in other languages?",
-      ans: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lore  ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus exsit amet blandit leo lobortis eget.",
+      ans: "Yes, it is also available in Spanish, and we are excited to expand to more languages soon. We believe that everyones deserves to have their story told regardless of their language.",
       panel: "panel3",
       isexpanded: false,
 
     },
     {
       qs: "Can I add photos?",
-      ans: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lore  ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus exsit amet blandit leo lobortis eget.",
+      ans: "Yes, we encourage adding as many photos as you like. You don’t have to worry about photo quality as we automatically upscale and fit your photos within printing requirements!",
       panel: "panel4",
       isexpanded: false,
     },
     {
       qs: "Can I purchase extra books?",
-      ans: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lore  ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus exsit amet blandit leo lobortis eget.",
+      ans: "Yes, additional full-color hardcover books are available for 39$ each during or after your subscription period.",
       panel: "panel5",
       isexpanded: false,
     },
     {
       qs: "How long will it take?",
-      ans: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lore  ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus exsit amet blandit leo lobortis eget.",
+      ans: "Your book, your pace! Some people finish their book in 2-3 sittings while others prefer to enjoy the process and continually polish their answers for months. However you choose, enjoy revisiting your unique memories!",
       panel: "panel6",
       isexpanded: false,
     },
     {
       qs: "What is your refund policy?",
-      ans: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lore  ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus exsit amet blandit leo lobortis eget.",
+      ans: "If you are not entirely satisfied with your Lifescript experience, we offer an easy, no-questions-asked refund within 30 days of purchase, as long as no books have been printed. Your happiness is our highest priority.",
       panel: "panel7",
       isexpanded: false,
     },
     {
       qs: "What happens when my subscription ends?",
-      ans: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lore  ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus exsit amet blandit leo lobortis eget.",
+      ans: "You will receive a reminder a month before the subscription ends. You will have a chance to complete your stories and order your book. If needed, we offer an additional month for free or to renew the subscription for another year.",
       panel: "panel8",
       isexpanded: false,
     },
