4a91b391d41973d7eb435cfe5785530246d07ce1
faheem4531 <faheem4531@gmail.com>
2024-04-05 14:24:00 +0500

    Main page renew

--- src/pages/dashboard/Questionnaire/index.tsx
+++ src/pages/dashboard/Questionnaire/index.tsx
@@ -3,44 +3,26 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import Profile from "../../../_assets/svg/profile.svg";
 import styles from "./Questionnaire.module.css";
+import Arrow from "@/_assets/svg/getStarted-aero.svg";
 
 const QuestionnaireLanding = () => {
   const router = useRouter();
   const { userName } = router.query;
+
   return (
-    <Box className={styles.QuestionnaireLandingMain}>
+    <Box className={styles.QuestionnaireLandingMain} onClick={() => {
+      router.push(
+        `/dashboard/Questionnaire/QuestionnaireLanding`
+      );
+    }}>
       <Box
         sx={{
-          width: "70%",
+          width: { sm: "70%", xs: "90%" },
           margin: "auto",
           textAlign: "center",
         }}
       >
-        <Box
-          sx={{
-            width: { md: "343px", sm: "200px", xs: "120px" },
-            height: { md: "343px", sm: "200px", xs: "120px" },
-            borderRadius: "50%",
-            margin: "0px auto 20px",
-          }}
-        >
-          <Image
-            src={Profile}
-            alt=""
-            style={{
-              width: "100%",
-              height: "100%",
-            }}
-          />
-        </Box>
-        <Typography
-          sx={{
-            fontSize: { md: "44px", sm: "36px", xs: "26px" },
-            fontWeight: 700,
-          }}
-        >
-          Hello, I am Alice
-        </Typography>
+
         <Box>
           <Typography
             sx={{
@@ -62,48 +44,13 @@ const QuestionnaireLanding = () => {
             }}
           >
             We are going to need your help answering a quick questionnaire that
-            would personalize your book questions and experience. Thank you for
+            would personalize your experience. Thank you for
             answering!
           </Typography>
         </Box>
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            mt: "30px",
-            justifyContent: "space-between",
-            width: { md: "80px", sm: "65px", xs: "60px" },
-            margin: "auto",
-            cursor: "pointer",
-          }}
-          onClick={() => {
-            router.push(
-              `/dashboard/Questionnaire/QuestionnaireLanding`
-            );
-          }}
-        >
-          <Typography
-            sx={{
-              fontSize: { md: "22px", sm: "16px", xs: "15px" },
-              fontWeight: 600,
-              color: "rgba(255, 255, 255, 0.60)",
-              textDecoration: "underline",
-            }}
-          >
-            Next
-          </Typography>
-          {/* <Image src={NextArrow} alt="NextArrow" /> */}
-          <span
-            style={{
-              fontSize: "20px",
-              fontWeight: 600,
-              color: "rgba(255, 255, 255, 0.60)",
-            }}
-          >
-            &#8594;
-          </span>
-        </Box>
+
       </Box>
+      <Image src={Arrow} alt='next' className={styles.nextAero} />
     </Box>
   );
 };
