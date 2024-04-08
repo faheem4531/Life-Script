3b169b6c8bc277f735006eddbc4ef21d2a4a60ac
faheem4531 <faheem4531@gmail.com>
2024-04-08 14:58:24 +0500

    set tab for setp 4

--- src/pages/dashboard/Questionnaire/qaTabBars.tsx
+++ src/pages/dashboard/Questionnaire/qaTabBars.tsx
@@ -5,12 +5,12 @@ const QaTabBars = ({ tabProp }) => {
     <Grid
       container
       gap={2.5}
-      sx={{ color: "black", width: "100%", maxWidth: "532px" }}
+      sx={{ width: "100%", maxWidth: "700px", }}
     >
       {/* Tab 1 */}
       <Grid
         item
-        xs={tabProp === 1 ? 5.5 : 2.5}
+        xs={tabProp === 1 ? 4 : 2.2}
         sx={{
           bgcolor: tabProp === 1 ? "#E1683B" : "#E1683B",
           borderRadius: "3px",
@@ -22,7 +22,7 @@ const QaTabBars = ({ tabProp }) => {
       {/* Tab 2 */}
       <Grid
         item
-        xs={tabProp === 2 ? 5.5 : 2.5}
+        xs={tabProp === 2 ? 4 : 2.2}
         sx={{
           bgcolor: tabProp === 2 ? "#E1683B" : "#E1683B",
           borderRadius: "3px",
@@ -34,12 +34,24 @@ const QaTabBars = ({ tabProp }) => {
       {/* Tab 3 */}
       <Grid
         item
-        xs={tabProp === 3 ? 5.5 : 2.5}
+        xs={tabProp === 3 ? 4 : 2.2}
         sx={{
           bgcolor: tabProp === 3 ? "#E1683B" : "#E1683B", // Change this color as needed
           borderRadius: "3px",
           height: "10px",
           opacity: tabProp === 3 ? "1" : "0.2",
+        }}
+      ></Grid>
+
+      {/* Tab 4 */}
+      <Grid
+        item
+        xs={tabProp === 4 ? 4 : 2.2}
+        sx={{
+          bgcolor: tabProp === 4 ? "#E1683B" : "#E1683B", // Change this color as needed
+          borderRadius: "3px",
+          height: "10px",
+          opacity: tabProp === 4 ? "1" : "0.2",
         }}
       ></Grid>
     </Grid>
