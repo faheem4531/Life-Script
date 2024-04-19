68298b278f7e5dcc87ee35926894f256e4e7c02a
asif-hanjra <de.asifalihanjra@gmail.com>
2024-04-19 17:52:32 +0500

    update update

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import FacebookProvider from "next-auth/providers/facebook";
 
 export default NextAuth({
-  // secret: "9Cg2vk+Rw31F0NJ/QDCr1JTA0NZoBaMWpW9uqNdSXwc=",
+  secret: "9Cg2vk+Rw31F0NJ/QDCr1JTA0NZoBaMWpW9uqNdSXwc=",
   providers: [
     FacebookProvider({
       // clientId: "1076442010181556",
