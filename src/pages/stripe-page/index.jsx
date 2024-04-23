4ce8349ca73ea976ab70c53c16733e2765b07ec6
faheem4531 <faheem4531@gmail.com>
2024-04-23 17:19:50 +0500

    gifting interphase fixes page 1

--- src/pages/stripe-page/index.jsx
+++ src/pages/stripe-page/index.jsx
@@ -1,23 +1,16 @@
-import { useState } from "react";
-import Button from "@/__webComponents/button/Button";
-import Logo from "@/_assets/svg/logo-dashboard.svg";
-import {
-  Box,
-  FormControl,
-  FormControlLabel,
-  Radio,
-  RadioGroup,
-  Typography,
-} from "@mui/material";
-import Head from "next/head";
-import Image from "next/image";
-import BgLogo from "../../_assets/svg/BgLogo.svg";
+import { useState } from 'react';
+import Button from '@/__webComponents/button/Button';
+import Logo from '@/_assets/svg/logo-dashboard.svg';
+import { Box, FormControl, FormControlLabel, Radio, RadioGroup, Typography } from '@mui/material';
+import Head from 'next/head';
+import Image from 'next/image';
+import BgLogo from '../../_assets/svg/BgLogo.svg';
 import { useRouter } from 'next/router';
 
 const StripePage = () => {
   const router = useRouter();
-  const [userType, setUserType] = useState("myself");
-  const [paymentType, setPaymentType] = useState("buynow");
+  const [userType, setUserType] = useState('myself');
+  const [paymentType, setPaymentType] = useState('buynow');
   const [showSecondForm, setShowSecondForm] = useState(false);
 
   const handleContinue = () => {
@@ -25,10 +18,10 @@ const StripePage = () => {
     // router.push("/stripe-page/subscription");
   };
   const handleContinueNext = () => {
-    if (paymentType === "free-trial") {
-      router.push("/stripe-page/gift-subscription");
+    if (paymentType === 'free-trial') {
+      router.push('/stripe-page/gift-subscription');
     } else {
-      router.push("/stripe-page/subscription");
+      router.push('/stripe-page/subscription');
     }
   };
 
@@ -41,20 +34,20 @@ const StripePage = () => {
 
       <Box
         sx={{
-          bgcolor: "#f3ecda",
-          color: "#3e4f3c",
-          height: "100vh",
-          position: "relative",
+          bgcolor: '#f3ecda',
+          color: '#3e4f3c',
+          height: '100vh',
+          position: 'relative',
         }}
       >
-        <Box position={"relative"} zIndex={2}>
+        <Box position={'relative'} zIndex={2}>
           <Box
             sx={{
-              bgcolor: "#30422e",
-              height: "50px",
-              display: "flex",
-              alignItems: "center",
-              color: "white",
+              bgcolor: '#30422e',
+              display: 'flex',
+              alignItems: 'center',
+              color: 'white',
+              padding: '26px 0 26px 17px',
             }}
           >
             <Image src={Logo} alt="Logo" />
@@ -62,30 +55,23 @@ const StripePage = () => {
 
           <Box
             sx={{
-              marginTop: "100px",
-              marginLeft: "100px",
-              width: "600px",
+              margin: { md: '100px 0 0 100px', sm: '100px 0 0 20px', xs: '60px 20px 0 ' },
+              maxWidth: '600px',
             }}
           >
-            <FormControl sx={{ display: showSecondForm ? "none" : "block" }}>
-              <Typography
-                sx={{ color: "#30422E", fontSize: "30px", margin: "10px" }}
-              >
-                Who is LifeScript for?
-              </Typography>
-              <RadioGroup
-                aria-labelledby="demo-radio-buttons-group-label"
-                value={userType}
-                onChange={(e) => setUserType(e.target.value)}
-                sx={{ padding: "20px" }}
-              >
+            <FormControl sx={{ display: showSecondForm ? 'none' : 'block' }}>
+              <Typography sx={{ color: '#30422E', fontSize: '30px' }}>Who is LifeScript for?</Typography>
+              <RadioGroup aria-labelledby="demo-radio-buttons-group-label" value={userType} onChange={(e) => setUserType(e.target.value)}>
                 <Box
                   sx={{
-                    backgroundColor: "white",
-                    margin: "10px",
-                    marginLeft: "-10px",
-                    border: "1px solid black",
-                    borderRadius: "4px",
+                    backgroundColor: 'white',
+                    margin: { sm: '70px 0 30px', xs: '50px 0 15px' },
+                    height: '68px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    // marginLeft: '-10px',
+                    border: '1px solid black',
+                    borderRadius: '4px',
                   }}
                 >
                   <FormControlLabel
@@ -93,23 +79,26 @@ const StripePage = () => {
                     control={
                       <Radio
                         sx={{
-                          "&.Mui-checked .MuiSvgIcon-root": {
-                            fill: "#30422E",
+                          '&.Mui-checked .MuiSvgIcon-root': {
+                            fill: '#30422E',
                           },
                         }}
                       />
                     }
                     label="Myself"
-                    sx={{ marginLeft: "10px" }}
+                    sx={{ marginLeft: '10px' }}
                   />
                 </Box>
                 <Box
                   sx={{
-                    backgroundColor: "white",
-                    margin: "10px",
-                    marginLeft: "-10px",
-                    border: "1px solid black",
-                    borderRadius: "4px",
+                    backgroundColor: 'white',
+                    // margin: '10px',
+                    // marginLeft: '-10px',
+                    height: '68px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    border: '1px solid black',
+                    borderRadius: '4px',
                   }}
                 >
                   <FormControlLabel
@@ -117,51 +106,43 @@ const StripePage = () => {
                     control={
                       <Radio
                         sx={{
-                          "&.Mui-checked .MuiSvgIcon-root": {
-                            fill: "#30422E",
+                          '&.Mui-checked .MuiSvgIcon-root': {
+                            fill: '#30422E',
                           },
                         }}
                       />
                     }
                     label="A gift of someone else"
-                    sx={{ marginLeft: "10px" }}
+                    sx={{ marginLeft: '10px' }}
                   />
                 </Box>
               </RadioGroup>
               {userType && (
-                <Box sx={{ margin: "10px" }}>
-                  <Button
-                    width="50%"
-                    height="50px"
-                    backgroundColor="#E1693B"
-                    title={"Continue"}
-                    bgHover="#B5522D"
-                    onClick={handleContinue}
-                  />
+                <Box
+                  sx={{
+                    marginTop: { sm: '40px', xs: '20px' },
+                    width: { sm: '300px', xs: '100%' },
+                  }}
+                >
+                  <Button width="100%" height="50px" backgroundColor="#E1693B" title={'Continue'} bgHover="#B5522D" onClick={handleContinue} />
                 </Box>
               )}
             </FormControl>
 
             {showSecondForm && (
-              <FormControl sx={{ display: "block" }}>
-                <Typography
-                  sx={{ color: "#30422E", fontSize: "30px", margin: "10px" }}
-                >
-                  Select Your Preference
-                </Typography>
-                <RadioGroup
-                  aria-labelledby="demo-radio-buttons-group-label"
-                  value={paymentType}
-                  onChange={(e) => setPaymentType(e.target.value)}
-                  sx={{ padding: "20px" }}
-                >
+              <FormControl sx={{ display: 'block' }}>
+                <Typography sx={{ color: '#30422E', fontSize: '30px', margin: '10px' }}>Select Your Preference</Typography>
+                <RadioGroup aria-labelledby="demo-radio-buttons-group-label" value={paymentType} onChange={(e) => setPaymentType(e.target.value)} sx={{ padding: '20px' }}>
                   <Box
                     sx={{
-                      backgroundColor: "white",
-                      margin: "10px",
-                      marginLeft: "-10px",
-                      border: "1px solid black",
-                      borderRadius: "4px",
+                      backgroundColor: 'white',
+                      margin: '10px',
+                      marginLeft: '-10px',
+                      height: '68px',
+                      display: 'flex',
+                      alignItems: 'center',
+                      border: '1px solid black',
+                      borderRadius: '4px',
                     }}
                   >
                     <FormControlLabel
@@ -169,23 +150,26 @@ const StripePage = () => {
                       control={
                         <Radio
                           sx={{
-                            "&.Mui-checked .MuiSvgIcon-root": {
-                              fill: "#30422E",
+                            '&.Mui-checked .MuiSvgIcon-root': {
+                              fill: '#30422E',
                             },
                           }}
                         />
                       }
                       label="Buy Now"
-                      sx={{ marginLeft: "10px" }}
+                      sx={{ marginLeft: '10px' }}
                     />
                   </Box>
                   <Box
                     sx={{
-                      backgroundColor: "white",
-                      margin: "10px",
-                      marginLeft: "-10px",
-                      border: "1px solid black",
-                      borderRadius: "4px",
+                      backgroundColor: 'white',
+                      margin: '10px',
+                      marginLeft: '-10px',
+                      height: '68px',
+                      display: 'flex',
+                      alignItems: 'center',
+                      border: '1px solid black',
+                      borderRadius: '4px',
                     }}
                   >
                     <FormControlLabel
@@ -193,50 +177,41 @@ const StripePage = () => {
                       control={
                         <Radio
                           sx={{
-                            "&.Mui-checked .MuiSvgIcon-root": {
-                              fill: "#30422E",
+                            '&.Mui-checked .MuiSvgIcon-root': {
+                              fill: '#30422E',
                             },
                           }}
                         />
                       }
                       label="Start 7-Day Free Trial"
-                      sx={{ marginLeft: "10px" }}
+                      sx={{ marginLeft: '10px' }}
                     />
                   </Box>
                 </RadioGroup>
 
-                <Box sx={{ margin: "10px" }}>
-                  <Button
-                    width="50%"
-                    height="50px"
-                    backgroundColor="#E1693B"
-                    title={"Continue"}
-                    bgHover="#B5522D"
-                    onClick={handleContinueNext}
-                  />
+                <Box sx={{ margin: '10px', width: { sm: '300px', xs: '100%' } }}>
+                  <Button width="100%" height="50px" backgroundColor="#E1693B" title={'Continue'} bgHover="#B5522D" onClick={handleContinueNext} />
                 </Box>
               </FormControl>
             )}
-
-
           </Box>
         </Box>
         <Box
           sx={{
-            position: "absolute",
-            right: "0px",
-            bottom: "0px",
+            position: 'absolute',
+            right: '0px',
+            bottom: '0px',
             zIndex: 1,
-            maxWidth: "768px",
-            width: "100%",
+            maxWidth: '768px',
+            width: '100%',
           }}
         >
           <Image
             src={BgLogo}
             alt="Giving Tree Logo"
             style={{
-              width: "100%",
-              height: "100%",
+              width: '100%',
+              height: '100%',
             }}
           />
         </Box>
