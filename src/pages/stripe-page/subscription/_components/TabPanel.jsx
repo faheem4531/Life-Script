1e9a6f0e3402d1d64d47339768a094aa27d34dec
faheem4531 <faheem4531@gmail.com>
2024-04-23 19:28:55 +0500

    subscription cards page foxes

--- src/pages/stripe-page/subscription/_components/TabPanel.jsx
+++ src/pages/stripe-page/subscription/_components/TabPanel.jsx
@@ -1,68 +1,81 @@
-'use client'
-import { Box } from "@mui/material";
-import styles from "../../../../__webComponents/ComponentsStyles.module.css"
-import PrimaryHeading from "../../../../__webComponents/headings/PrimaryHeading";
-import NewPricingCard from "./NewPricingCard";
+'use client';
+import { Box, Typography } from '@mui/material';
+import styles from '../../../../__webComponents/ComponentsStyles.module.css';
+import PrimaryHeading from '../../../../__webComponents/headings/PrimaryHeading';
+import NewPricingCard from './NewPricingCard';
 
-const TabPanel = ({onClick,selectedTab}) => {
+const TabPanel = ({ onClick, selectedTab }) => {
   const pricingCard = [
     {
-      id: "basic",
-      category: "Basic",
-      price: "$139",
-      card: "1"
+      id: 'basic',
+      category: 'Basic',
+      price: '$139',
+      card: '1',
     },
     {
-      id: "standard",
-      category: "Standard",
-      price: "$179",
-      card: "2"
+      id: 'standard',
+      category: 'Standard',
+      price: '$179',
+      card: '2',
     },
     {
-      id: "primium",
-      category: "Premium",
-      price: "$239",
-      card: "3"
+      id: 'primium',
+      category: 'Premium',
+      price: '$239',
+      card: '3',
     },
-  ]
-  function handleHover(id) {
-
-  }
+  ];
+  function handleHover(id) {}
 
   return (
-    <Box sx={{
-    //   margin: { lg: '170px 95px  130px', sm: "150px 30px", xs: "80px 20px 100px" },
-    }}>
-  <Box>
-          <PrimaryHeading heading="Choose a Subscription Plan" />
-
-          <Box
+    <Box
+      sx={
+        {
+          //   margin: { lg: '170px 95px  130px', sm: "150px 30px", xs: "80px 20px 100px" },
+        }
+      }
+    >
+      <Box>
+        <Box
+          sx={{
+            marginTop: { xs: '15px' },
+            fontSize: { sm: '15px', xs: '16px' },
+            // width: { sm: '60%', xs: '90%' },
+            marginLeft: { sm: '75px', xs: '20px' },
+          }}
+        >
+          <Typography
             sx={{
-              marginTop: { xs: "15px" },
-              fontSize: { sm: "15px", xs: "16px" },
-              width: { sm: "60%", xs: "90%" },
-              fontFamily: "Avenir",
-              marginLeft: "150px",
+              fontSize: { md: '52px', sm: '44px', xs: '32px' },
+              fontFamily: 'Avenir5 !important',
             }}
           >
-            Each package comes with a one-year LifeScript subscription, a
-            premium full-color hardcover book, and free shipping.
-          </Box>
+            Choose a Subscription Plan
+          </Typography>
+          <Typography sx={{ margin: { sm: '40px 0 20px', xs: '20px 0' } }}>
+            Each package comes with a one-year LifeScript subscription, a premium full-color hardcover book, and free shipping.
+          </Typography>
         </Box>
-      <Box sx={{ display: "flex", flexDirection: { md: "row", sm: "column", xs: "column" }, alignItems: "center", columnGap: "17px", rowGap: "50px", justifyContent: "center" }} className={styles.cardsMain} 
-      onClick={() => onClick(selectedTab + 1 )}
-      >
-        {pricingCard.map((item) => <NewPricingCard
-          key={item.id}
-          category={item.category}
-          price={item.price}
-          card={item.card}
-          id={item.id}
-          handleHover={handleHover}
-        />)}
       </Box>
-    </Box >
-  )
-}
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: { md: 'row', sm: 'column', xs: 'column' },
+          alignItems: 'center',
+          columnGap: { lg: '50px', sm: '20px' },
+          rowGap: '50px',
+          justifyContent: 'center',
+          padding: ' 0 40px 70px',
+        }}
+        className={styles.cardsMain}
+        onClick={() => onClick(selectedTab + 1)}
+      >
+        {pricingCard.map((item) => (
+          <NewPricingCard key={item.id} category={item.category} price={item.price} card={item.card} id={item.id} handleHover={handleHover} />
+        ))}
+      </Box>
+    </Box>
+  );
+};
 
 export default TabPanel;
--- src/pages/stripe-page/subscription/index.jsx
+++ src/pages/stripe-page/subscription/index.jsx
@@ -1,15 +1,15 @@
-"use client";
-import Logo from "@/_assets/svg/logo-dashboard.svg";
-import { Box } from "@mui/material";
-import Image from "next/image";
-import { useEffect, useState } from "react";
-import PurchaseForm from "./_components/PurchaseForm";
-import RegisterPage from "./_components/RegisterPage";
-import TabPanel from "./_components/TabPanel";
-import NewTabBar from "./_components/NewTabBar";
-import { useSession } from "next-auth/react";
-import { facebookLogin } from "@/store/slices/authSlice";
-import { useDispatch } from "react-redux";
+'use client';
+import Logo from '@/_assets/svg/logo-dashboard.svg';
+import { Box } from '@mui/material';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import PurchaseForm from './_components/PurchaseForm';
+import RegisterPage from './_components/RegisterPage';
+import TabPanel from './_components/TabPanel';
+import NewTabBar from './_components/NewTabBar';
+import { useSession } from 'next-auth/react';
+import { facebookLogin } from '@/store/slices/authSlice';
+import { useDispatch } from 'react-redux';
 
 const SubscriptionPage = () => {
   const dispatch = useDispatch();
@@ -22,7 +22,7 @@ const SubscriptionPage = () => {
         setSelectedTab(2);
         const payload = {
           name: session.user.name,
-          email: session.user.email
+          email: session.user.email,
         };
         dispatch(facebookLogin(payload));
       }
@@ -34,53 +34,49 @@ const SubscriptionPage = () => {
   };
 
   const tabsData = [
-    { label: "CHOOSE PLAN", active: selectedTab === 0 },
-    { label: "REGISTER", active: selectedTab === 1 && !session },
-    { label: "PAYMENT", active: selectedTab === 2 }
+    { label: 'CHOOSE PLAN', active: selectedTab === 0 },
+    { label: 'REGISTER', active: selectedTab === 1 && !session },
+    { label: 'PAYMENT', active: selectedTab === 2 },
   ];
 
   return (
     <>
       <Box
         sx={{
-          bgcolor: "#f3ecda",
-          color: "#3e4f3c",
+          bgcolor: '#f3ecda',
+          color: '#3e4f3c',
         }}
       >
         <Box
           sx={{
-            bgcolor: "#30422e",
-            height: "50px",
-            display: "flex",
-            alignItems: "center",
-            color: "white",
+            bgcolor: '#30422e',
+            padding: '26px 0 26px 15px',
+            display: 'flex',
+            alignItems: 'center',
+            color: 'white',
           }}
         >
           <Image src={Logo} alt="Logo" />
         </Box>
 
-        <Box sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          width: "100%",
-          marginTop: "30px",
-          marginLeft: "80px"
-        }}>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '100%',
+            marginTop: '70px',
+            marginLeft: { sm: '70px', xs: '20px' },
+          }}
+        >
           <NewTabBar tabs={tabsData} onClick={handleTabClick} />
         </Box>
 
         <Box>
-          <Box mt="15px">
-            {selectedTab === 0 && (
-              <TabPanel selectedTab={selectedTab} onClick={handleTabClick} />
-            )}
-            {selectedTab === 1 && !session && (
-              <RegisterPage selectedTab={selectedTab} onClick={handleTabClick} />
-            )}
-            {selectedTab === 2 && (
-              <PurchaseForm selectedTab={selectedTab} onClick={handleTabClick} />
-            )}
+          <Box mt="60px">
+            {selectedTab === 0 && <TabPanel selectedTab={selectedTab} onClick={handleTabClick} />}
+            {selectedTab === 1 && !session && <RegisterPage selectedTab={selectedTab} onClick={handleTabClick} />}
+            {selectedTab === 2 && <PurchaseForm selectedTab={selectedTab} onClick={handleTabClick} />}
           </Box>
         </Box>
       </Box>
