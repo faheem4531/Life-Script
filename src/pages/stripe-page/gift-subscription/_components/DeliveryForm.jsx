8996175c8b3a435899b29611348ea7f755973341
faheem4531 <faheem4531@gmail.com>
2024-04-24 16:39:55 +0500

    dilivary plan adjustments and fixes

--- src/pages/stripe-page/gift-subscription/_components/DeliveryForm.jsx
+++ src/pages/stripe-page/gift-subscription/_components/DeliveryForm.jsx
@@ -1,18 +1,12 @@
-"use client";
-import {
-  Box,
-  Button,
-  Divider,
-  TextField,
-  Typography,
-} from "@mui/material";
-import { useRouter } from "next/router";
-import { useState } from "react";
-import GiftPlanCard from "./GiftPlanCard";
-import { DatePicker } from "@mui/x-date-pickers";
+'use client';
+import { Box, Button, Divider, TextField, Typography } from '@mui/material';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import GiftPlanCard from './GiftPlanCard';
+import { DatePicker } from '@mui/x-date-pickers';
 const DeliveryForm = ({ onClick, selectedTab }) => {
-    const [selectedDate, setSelectedDate] = useState(null);
-    const maxDate = new Date();
+  const [selectedDate, setSelectedDate] = useState(null);
+  const maxDate = new Date();
   const router = useRouter();
   const { price, category } = router.query;
 
@@ -33,58 +27,65 @@ const DeliveryForm = ({ onClick, selectedTab }) => {
   };
 
   return (
-    <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-      <Box className={"Container"} sx={{ width: "40%", marginLeft: "100px" }}>
+    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+      <Box className={'Container'} sx={{ width: '100%', maxWidth: '1370px', margin: { sm: '0 0 30px 70px', xs: '0 20px 30px 20px' } }}>
         <Box>
-          <Typography sx={{ fontSize: "40px", marginBottom: "20px" }}>
-            Personalize Your Gift
-          </Typography>
+          <Typography sx={{ fontSize: '40px', marginBottom: '20px' }}>Personalize Your Gift</Typography>
         </Box>
 
-        <Box>
-          <Box>
-            <Typography
-              sx={{
-                color: "#30422E",
-                fontSize: { xs: 12, sm: 14, md: 16, lg: 12 },
-              }}
-            >
-              Recipient’s Full Name
-            </Typography>
-            <TextField
-              variant="outlined"
-              placeholder="Enter Recipient’s full name"
-              name="name"
-              sx={{
-                marginBottom: "10px",
-                width: "100%",
-                bgcolor: "white",
-              }}
-            />
-          </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            width: '100%',
+            columnGap: '50px',
+          }}
+        >
+          <Box sx={{ maxWidth: { sm: '600px', xs: '100%' }, width: '100%' }}>
+            <Box>
+              <Box>
+                <Typography
+                  sx={{
+                    color: '#30422E',
+                    fontSize: { xs: 12, sm: 14, md: 16, lg: 12 },
+                  }}
+                >
+                  Recipient’s Full Name
+                </Typography>
+                <TextField
+                  variant="outlined"
+                  placeholder="Enter Recipient’s full name"
+                  name="name"
+                  sx={{
+                    marginBottom: '30px',
+                    width: '100%',
+                    bgcolor: 'white',
+                  }}
+                />
+              </Box>
 
-          <Box>
-            <Typography
-              sx={{
-                color: "#30422E",
-                fontSize: { xs: 12, sm: 14, md: 16, lg: 12 },
-              }}
-            >
-              Recipient’s Email
-            </Typography>
-            <TextField
-              variant="outlined"
-              placeholder="Enter Recipient’s email address"
-              name="email"
-              sx={{
-                marginBottom: "10px",
-                width: "100%",
-                bgcolor: "white",
-              }}
-            />
-          </Box>
+              <Box>
+                <Typography
+                  sx={{
+                    color: '#30422E',
+                    fontSize: { xs: 12, sm: 14, md: 16, lg: 12 },
+                  }}
+                >
+                  Recipient’s Email
+                </Typography>
+                <TextField
+                  variant="outlined"
+                  placeholder="Enter Recipient’s email address"
+                  name="email"
+                  sx={{
+                    marginBottom: '30px',
+                    width: '100%',
+                    bgcolor: 'white',
+                  }}
+                />
+              </Box>
 
-          {/* <Box>
+              {/* <Box>
             <Typography
               sx={{
                 color: "#30422E",
@@ -104,101 +105,103 @@ const DeliveryForm = ({ onClick, selectedTab }) => {
               }}
             />
           </Box> */}
-          <Box flex={1}>
-              <Typography
-                 sx={{
-                    color: "#30422E",
+              <Box>
+                <Typography
+                  sx={{
+                    color: '#30422E',
                     fontSize: { xs: 12, sm: 14, md: 16, lg: 12 },
                   }}
-              >
-               Send my gift on:
-              </Typography>
-              <DatePicker
-                value={selectedDate}
-                onChange={(date) => setSelectedDate(date)}
-                maxDate={maxDate}
-                sx={{
-                  width: "100%",
-                  backgroundColor: "white",
-                  borderRadius: "2px",
-                  "& .MuiOutlinedInput-root": {
-                    "& fieldset": {
-                      borderRadius: "2px",
+                >
+                  Send my gift on:
+                </Typography>
+                <DatePicker
+                  value={selectedDate}
+                  onChange={(date) => setSelectedDate(date)}
+                  maxDate={maxDate}
+                  sx={{
+                    width: '100%',
+                    backgroundColor: 'white',
+                    borderRadius: '2px',
+                    '& .MuiOutlinedInput-root': {
+                      '& fieldset': {
+                        borderRadius: '2px',
+                      },
                     },
-                  },
-                }}
-              />
+                  }}
+                />
+              </Box>
             </Box>
-        </Box>
 
-        <Box>
-          <Divider sx={{ width: "80%", marginTop: "30px" }} />
+            <Box>
+              <Divider sx={{ width: '100%', marginTop: '30px' }} />
 
-          <Typography
-            sx={{ marginTop: "30px", marginBottom: "10px", fontSize: "20px" }}
+              <Typography sx={{ marginTop: '30px', marginBottom: '30px', fontSize: '20px' }}>Add a gift message!</Typography>
+
+              <Box>
+                <Typography
+                  sx={{
+                    color: '#30422E',
+                    fontSize: { xs: 12, sm: 14, md: 16, lg: 12 },
+                  }}
+                >
+                  From
+                </Typography>
+                <TextField
+                  variant="outlined"
+                  placeholder="Your name and all others taking part of this gift."
+                  name="from"
+                  sx={{
+                    marginBottom: '30px',
+                    width: '100%',
+                    bgcolor: 'white',
+                  }}
+                />
+              </Box>
+              <Box>
+                <Typography
+                  sx={{
+                    color: '#30422E',
+                    fontSize: { xs: 12, sm: 14, md: 16, lg: 12 },
+                  }}
+                >
+                  Your Message
+                </Typography>
+                <TextField
+                  placeholder="Hello, I've gifted you a LifeScript subscription, allowing you to easily share and preserve your stories in a beautiful hardcover book."
+                  multiline
+                  rows={7}
+                  maxRows={10}
+                  sx={{ width: '100%', backgroundColor: '#FAFAFA' }}
+                />
+              </Box>
+            </Box>
+
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              onClick={() => onClick(selectedTab + 1)}
+              sx={{
+                width: '200px',
+                marginTop: '50px',
+                bgcolor: '#e1693b',
+                '&:hover': {
+                  backgroundColor: '#b5522d',
+                },
+              }}
+            >
+              Continue
+            </Button>
+          </Box>
+          <Box
+            sx={{
+              margin: '0 35px 35px 0',
+              display: { md: 'block', sm: 'none', xs: 'none' },
+            }}
           >
-            Add a gift message!
-          </Typography>
-
-          <Box>
-            <Typography
-              sx={{
-                color: "#30422E",
-                fontSize: { xs: 12, sm: 14, md: 16, lg: 12 },
-              }}
-            >
-              From
-            </Typography>
-            <TextField
-              variant="outlined"
-              placeholder="Your name and all others taking part of this gift."
-              name="from"
-              sx={{
-                marginBottom: "10px",
-                width: "100%",
-                bgcolor: "white",
-              }}
-            />
-          </Box>
-          <Box>
-            <Typography
-              sx={{
-                color: "#30422E",
-                fontSize: { xs: 12, sm: 14, md: 16, lg: 12 },
-              }}
-            >
-              Your Message
-            </Typography>
-            <TextField
-              placeholder="Hello, I've gifted you a LifeScript subscription, allowing you to easily share and preserve your stories in a beautiful hardcover book."
-              multiline
-              rows={7}
-              maxRows={10}
-              sx={{width:"575px", backgroundColor:"#FAFAFA"}}
-            />
+            <GiftPlanCard price={price} category={category} />
           </Box>
         </Box>
-
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          onClick={() => onClick(selectedTab + 1)}
-          sx={{
-            width: "200px",
-            marginTop: "50px",
-            bgcolor: "#e1693b",
-            "&:hover": {
-              backgroundColor: "#b5522d",
-            },
-          }}
-        >
-          Continue
-        </Button>
-      </Box>
-
-      <Box>
-        <GiftPlanCard price={price} category={category} />
       </Box>
     </Box>
   );
