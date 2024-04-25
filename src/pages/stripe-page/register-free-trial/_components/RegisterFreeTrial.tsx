763320d693b53ae6a668552d42bf253bc59c991f
faheem4531 <faheem4531@gmail.com>
2024-04-25 18:30:40 +0500

    formik validation or api binding

--- src/pages/stripe-page/register-free-trial/_components/RegisterFreeTrial.tsx
+++ src/pages/stripe-page/register-free-trial/_components/RegisterFreeTrial.tsx
@@ -4,24 +4,26 @@ import { Box, Button, Divider, TextField, Typography } from "@mui/material";
 import Image from "next/image";
 import facebookIcon from "../../../../../public/facebookIcon.svg";
 import googleLogo from "../../../../../public/googleIcon.svg";
-
 import {
-  googleSignup
+  googleSignup,
+  signup
 } from "@/store/slices/authSlice";
 import { useGoogleLogin } from "@react-oauth/google";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { toast } from "react-toastify";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useDispatch } from "react-redux";
-import { toast } from "react-toastify";
-
-
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { SignupData } from "@/interface/authInterface";
+import PaymentProcessingModal from '../../subscription/_components/Modal';
 
 const RegisterFreeTrial = () => {
-
+  const [openModal, setOpenModal] = useState(false);
   const router = useRouter();
   const { t } = useTranslation();
-
   const dispatch: any = useDispatch();
   const { data: session } = useSession();
   console.log("Session Check ", session)
@@ -91,172 +93,222 @@ const RegisterFreeTrial = () => {
     toast.error(t("signup-page.failedSignupGoogle"));
   };
 
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      name: "",
+    },
+    onSubmit: async (data: SignupData) => {
+      console.log(data, "dataaaaa");
+
+      dispatch(signup(data))
+        .unwrap()
+        .then(() => {
+          toast.success(t("signup-page.verificationEmailSent"));
+          setOpenModal(true)
+          // router.push(`/verify/verificationSent/?email=${data.email}`);
+        })
+        .catch((error: any) => {
+          toast.error(error?.message || t("signup-page.failedSignup"));
+        });
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email().required(t("signup-page.emailRequired")),
+      name: Yup.string().required(t("signup-page.nameRequired")),
+    }),
+  });
+
   return (
-    <Box
-      sx={{ border: "2px soild green", width: "100%" }}
-    >
-      <Typography variant="h4" sx={{ marginBottom: "60px" }}>
-        Register LifeScript
-      </Typography>
 
-      <Box sx={{ maxWidth: "600px", width: "100%" }}>
-        <Box>
-          <Typography
-            sx={{
-              color: "#30422E",
-              fontSize: { xs: 12, sm: 14, md: 16, lg: 16 },
-            }}
-          >
-            Name
-          </Typography>
-          <TextField
-            variant="outlined"
-            placeholder="Enter your full name"
-            name="name"
-            sx={{
-              marginBottom: "30px",
-              width: "100%",
-              bgcolor: "white",
-            }}
-          />
-        </Box>
+    <>
+      <Box
+        sx={{ border: "2px soild green", width: "100%" }}
+      >
+        <Typography variant="h4" sx={{ marginBottom: "60px" }}>
+          Register LifeScript
+        </Typography>
 
-        <Box>
-          <Typography
-            sx={{
-              color: "#30422E",
-              fontSize: { xs: 12, sm: 14, md: 16, lg: 16 },
-            }}
-          >
-            Email
-          </Typography>
-          <TextField
-            variant="outlined"
-            placeholder="Enter your email address"
-            name="email"
-            sx={{
-              marginBottom: "30px",
-              width: "100%",
-              bgcolor: "white",
-            }}
-          />
-        </Box>
-
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center"
-          }}
-        >
+        <Box sx={{ maxWidth: "600px", width: "100%" }}>
           <Box>
-            <Divider
+            <Typography
               sx={{
-                backgroundColor: "black",
-                width: { md: "270px", sm: "210px", xs: "130px" },
+                color: "#30422E",
+                fontSize: { xs: 12, sm: 14, md: 16, lg: 16 },
               }}
-              orientation="horizontal"
-            />
-          </Box>
-          <Box>
-            <Typography sx={{ margin: "0 20px", color: "#0000006B" }}>
-              {"or"}
+            >
+              Name
             </Typography>
-          </Box>
-          <Box>
-            <Divider
+            <TextField
+              variant="outlined"
+              placeholder="Enter your full name"
+              name="name"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
               sx={{
-                backgroundColor: "black",
-                width: { md: "270px", sm: "210px", xs: "130px" },
+                marginBottom: "10px",
+                width: "100%",
+                bgcolor: "white",
               }}
-              orientation="horizontal"
             />
           </Box>
-        </Box>
+          {formik.touched.name && formik.errors.name && (
+            <span style={{ color: "red" }}>{formik.errors.name}</span>
+          )}
 
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            gap: 1,
-            justifyContent: "center",
-          }}
-        >
           <Box>
-            <Button
-              variant="contained"
-              type="submit"
-              onClick={() => handleGoogleLogin()}
+            <Typography
               sx={{
-                borderRadius: "2px",
-                backgroundColor: "#fff",
-                padding: "10px 0",
                 color: "#30422E",
-                width: "100%",
-                height: "60px",
-                gap: "30px",
-                marginTop: { xs: "20px" },
-                "&:hover": {
-                  backgroundColor: "white",
-                },
-                textTransform: "capitalize",
+                mt: "30px",
+                fontSize: { xs: 12, sm: 14, md: 16, lg: 16 },
               }}
             >
-              <Image
-                src={googleLogo}
-                alt="Google Logo"
-              />
-              <Typography>Login with Google</Typography>
-            </Button>
-          </Box>
-
-          {/* Login With Facebook */}
-          <Box>
-            <Button
-              variant="contained"
-              // type="submit"
+              Email
+            </Typography>
+            <TextField
+              variant="outlined"
+              placeholder="Enter your email address"
+              name="email"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
               sx={{
-                borderRadius: "2px",
-                backgroundColor: "#fff",
-                padding: "10px 0",
-                color: "#30422E",
+                marginBottom: "10px",
                 width: "100%",
-                height: "60px",
-                gap: "30px",
-                marginTop: { xs: "20px" },
-                "&:hover": {
-                  backgroundColor: "white",
-                },
-                textTransform: "capitalize",
+                bgcolor: "white",
               }}
-              onClick={handleSignin}
-            >
-              <Image
-                src={facebookIcon}
-                alt="Facebook Logo"
-              />
-              <Typography>Login with Facebook</Typography>
-            </Button>
+            />
           </Box>
-        </Box>
+          {formik.touched.email && formik.errors.email && (
+            <span style={{ color: "red" }}>{formik.errors.email}</span>
+          )}
 
-        <Button
-          variant="contained"
-          color="primary"
-          sx={{
-            width: "260px",
-            marginTop: "60px",
-            height: "50px",
-            bgcolor: "#e1693b",
-            "&:hover": {
-              backgroundColor: "#b5522d",
-            },
-          }}
-        >
-          Start 7-Day Free Trial
-        </Button>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              mt: "30px"
+            }}
+          >
+            <Box>
+              <Divider
+                sx={{
+                  backgroundColor: "black",
+                  width: { md: "270px", sm: "210px", xs: "130px" },
+                }}
+                orientation="horizontal"
+              />
+            </Box>
+            <Box>
+              <Typography sx={{ margin: "0 20px", color: "#0000006B" }}>
+                {"or"}
+              </Typography>
+            </Box>
+            <Box>
+              <Divider
+                sx={{
+                  backgroundColor: "black",
+                  width: { md: "270px", sm: "210px", xs: "130px" },
+                }}
+                orientation="horizontal"
+              />
+            </Box>
+          </Box>
+
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: 1,
+              justifyContent: "center",
+            }}
+          >
+            <Box>
+              <Button
+                variant="contained"
+                type="submit"
+                onClick={() => handleGoogleLogin()}
+                sx={{
+                  borderRadius: "2px",
+                  backgroundColor: "#fff",
+                  padding: "10px 0",
+                  color: "#30422E",
+                  width: "100%",
+                  height: "60px",
+                  gap: "30px",
+                  marginTop: { xs: "20px" },
+                  "&:hover": {
+                    backgroundColor: "white",
+                  },
+                  textTransform: "capitalize",
+                }}
+              >
+                <Image
+                  src={googleLogo}
+                  alt="Google Logo"
+                />
+                <Typography>Login with Google</Typography>
+              </Button>
+            </Box>
+
+            {/* Login With Facebook */}
+            <Box>
+              <Button
+                variant="contained"
+                // type="submit"
+                sx={{
+                  borderRadius: "2px",
+                  backgroundColor: "#fff",
+                  padding: "10px 0",
+                  color: "#30422E",
+                  width: "100%",
+                  height: "60px",
+                  gap: "30px",
+                  marginTop: { xs: "20px" },
+                  "&:hover": {
+                    backgroundColor: "white",
+                  },
+                  textTransform: "capitalize",
+                }}
+                onClick={handleSignin}
+              >
+                <Image
+                  src={facebookIcon}
+                  alt="Facebook Logo"
+                />
+                <Typography>Login with Facebook</Typography>
+              </Button>
+            </Box>
+          </Box>
+
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={(event) => formik.handleSubmit()}
+            type="submit"
+            sx={{
+              width: "260px",
+              marginTop: "60px",
+              height: "50px",
+              bgcolor: "#e1693b",
+              "&:hover": {
+                backgroundColor: "#b5522d",
+              },
+            }}
+          >
+            Start 7-Day Free Trial
+          </Button>
+        </Box>
       </Box>
-    </Box>
+
+      <PaymentProcessingModal
+        openModal={openModal}
+        selectedTab={"verify"}
+        handleClose={() => setOpenModal(true)}
+      />
+
+    </>
+
   );
 };
 
