2f447e46c493127f4b1e98fee4095a645099fde0
tayyab-103 <tayyabhameed103@gmail.com>
2024-04-30 09:49:38 +0500

    integrate Google Translate in my app

--- src/__webComponents/navBar/Language.js
+++ src/__webComponents/navBar/Language.js
@@ -1,15 +1,16 @@
-'use client'
+"use client";
 
-import * as React from 'react';
-import { Select as BaseSelect } from '@mui/base/Select';
-import { Option as BaseOption } from '@mui/base/Option';
-import DownAero from "@/__webAssets/svgs/dropdown.svg"
-import EN from "@/__webAssets/svgs/EN.svg"
-import SP from "@/__webAssets/svgs/SP.svg"
-import Image from 'next/image';
-import { Box } from '@mui/material';
-import styles from "./NavBar.module.css"
-import Check from "@/__webAssets/svgs/check.svg"
+import * as React from "react";
+// import { Select as BaseSelect } from "@mui/base/Select";
+// import { Option as BaseOption } from "@mui/base/Option";
+// import DownAero from "@/__webAssets/svgs/dropdown.svg";
+// import EN from "@/__webAssets/svgs/EN.svg";
+// import SP from "@/__webAssets/svgs/SP.svg";
+// import Image from "next/image";
+import { Box } from "@mui/material";
+import styles from "./NavBar.module.css";
+// import Check from "@/__webAssets/svgs/check.svg";
+import Script from "next/script";
 
 export default function LanguageOption() {
   const [selectedValue, setSelectedValue] = React.useState(10); // Initial selected value
@@ -25,37 +26,90 @@ export default function LanguageOption() {
 
   return (
     <Box sx={{ position: "relative" }} onClick={handleToggle}>
-      <BaseSelect
+      {/* Google Translate script */}
+      <Script
+        id="google_translate_element"
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `
+            function googleTranslateElementInit() {
+              new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+            }
+          `,
+        }}
+      />
+      <Script
+        type="text/javascript"
+        src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+      />
+
+      <Box
+        id="google_translate_element"
+        className={styles.google}
+        sx={{ backgroundColor: "#f3ecda" }}
+      >
+        <Box id="goog-gt-tt" class="goog-tooltip skiptranslate" ></Box>
+      </Box>
+    </Box>
+  );
+}
+
+{
+  /* <BaseSelect
         value={selectedValue}
         className={styles.selector}
         onChange={handleChange}
       >
-        <BaseOption value={10} className={`${styles.option} ${styles.optiontop} ${selectedValue == 10 && styles.optionsBg}`}>
+        <BaseOption
+          value={10}
+          className={`${styles.option} ${styles.optiontop} ${
+            selectedValue == 10 && styles.optionsBg
+          }`}
+        >
           EN
-          <Box sx={{
-            borderRadius: "50%", width: "36px", height: "36px", position: "absolute", left: "15px", top: "17px",
-          }}>
+          <Box
+            sx={{
+              borderRadius: "50%",
+              width: "36px",
+              height: "36px",
+              position: "absolute",
+              left: "15px",
+              top: "17px",
+            }}
+          >
             <Image src={EN} alt="English" width={25} height={25} />
           </Box>
-          {selectedValue == 10 &&
+          {selectedValue == 10 && (
             <Box sx={{ position: "absolute", right: "15px", bottom: "20px" }}>
               <Image src={Check} alt="check" width={17} />
-            </Box>}
+            </Box>
+          )}
         </BaseOption>
-        <BaseOption value={20} className={`${styles.option} ${selectedValue == 20 && styles.optionsBg}`}>
+        <BaseOption
+          value={20}
+          className={`${styles.option} ${
+            selectedValue == 20 && styles.optionsBg
+          }`}
+        >
           ES
-          <Box sx={{ borderRadius: "50%", width: "36px", height: "36px", position: "absolute", left: "15px", bottom: "8px" }}>
+          <Box
+            sx={{
+              borderRadius: "50%",
+              width: "36px",
+              height: "36px",
+              position: "absolute",
+              left: "15px",
+              bottom: "8px",
+            }}
+          >
             <Image src={SP} alt="Spanish" width={25} height={25} />
           </Box>
-
-          {selectedValue == 20 && <Box sx={{ position: "absolute", right: "15px", bottom: "20px" }}>
-            <Image src={Check} alt="check" width={17} />
-          </Box>}
+          {selectedValue == 20 && (
+            <Box sx={{ position: "absolute", right: "15px", bottom: "20px" }}>
+              <Image src={Check} alt="check" width={17} />
+            </Box>
+          )}
         </BaseOption>
-
-      </BaseSelect >
-      <Image src={DownAero} alt="aero" width={13} className={styles.downAero} />
-
-    </Box>
-  );
+      </BaseSelect>
+      <Image src={DownAero} alt="aero" width={13} className={styles.downAero} /> */
 }
