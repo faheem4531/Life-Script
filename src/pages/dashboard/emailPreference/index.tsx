c55c0d86067fa30b7d05ca2aa30b942743c462a6
faheem4531 <faheem4531@gmail.com>
2024-05-02 20:30:09 +0500

    email preference api binding

--- src/pages/dashboard/emailPreference/index.tsx
+++ src/pages/dashboard/emailPreference/index.tsx
@@ -4,32 +4,36 @@ import GlobelBtn from "@/components/button/Button";
 import SubscriptionHeader from "@/components/dashboardComponent/subscriptionHeader";
 import CustomizationDialog from "@/components/modal/CustomizationDialog";
 import { customerSupport } from "@/store/slices/chatSlice";
-import { Box, TextField, Typography } from "@mui/material";
+import { Box, Checkbox, FormControlLabel, RadioGroup, Typography } from "@mui/material";
+import Radio from "@mui/material/Radio";
 import Image from "next/image";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useDispatch } from "react-redux";
 import { toast } from "react-toastify";
 import AddChapterName from '@/components/dashboardComponent/AddChapterName';
+import { getUserProfile, updateUserProfile } from "@/store/slices/authSlice";
+
 
 const EmailPreference = () => {
-  // const dispatch: any = useDispatch();
-  // const [subject, setSubject] = useState("");
-  // const [description, setDescription] = useState("");
-  // const [showTooltip, setShowTooltip] = useState(false);
-  // const [showModal, setShowModal] = useState(false);
-  // const { t } = useTranslation();
+  const [questionFrequency, setQuestionFrequency] = useState("ONEDAY");
 
-  // const handleComplaint = () => {
-  //   dispatch(customerSupport({ subject: subject, description: description }))
-  //     .unwrap()
-  //     .then(() => {
-  //       setShowModal(true);
-  //       setDescription("");
-  //       setSubject("");
-  //     })
-  //     .catch(() => toast.error("Failed to create your ticket"));
-  // };
+  const handleQuestionFrequency = (event) => {
+    setQuestionFrequency(event.target.value);
+  };
+
+  const dispatch: any = useDispatch();
+  // const { t } = useTranslation();;
+
+
+  const handleSubmit = () => {
+    dispatch(
+      updateUserProfile({
+        questionAskType: questionFrequency,
+      }))
+      .then(() => toast.success("updated successfully"))
+      .catch(() => toast.error("Failed to update"));
+  }
 
   return (
     <Box>
@@ -44,19 +48,228 @@ const EmailPreference = () => {
             chapter="Email Preferences"
             title="support"
           />
-          <Box
-            sx={{
-              display: "flex",
-              gap: "30px",
-              borderRadius: "4x",
-              bgcolor: "#F3ECDA",
-              p: { md: "30px", sm: "20px 20px", xs: "16px" },
-              mt: "26px",
-              flexWrap: "wrap",
-            }}
-          >
-
+          <Box sx={{
+            borderRadius: "4x",
+            bgcolor: "#F3ECDA",
+            p: { md: "30px", sm: "20px 20px", xs: "16px" },
+            mt: "26px",
+          }}>
+            <Typography
+              sx={{
+                fontSize: { sm: "24px", xs: "20px" },
+                fontWeight: 700,
+                color: "#30422E",
+              }}
+            >
+              How often would you like to receive questions via email?
+            </Typography>
+            <Box sx={{ mt: 2, gap: 2 }}>
+              <RadioGroup
+                sx={{
+                  display: 'flex',
+                  flexDirection: { md: 'row', xs: "column", sm: "column" },
+                  gap: '16px',
+                }}
+                value={questionFrequency}
+                onChange={handleQuestionFrequency}
+              >
+                <FormControlLabel
+                  value="ONEDAY"
+                  checked={questionFrequency === "ONEDAY"}
+                  control={
+                    <Radio
+                      sx={{
+                        "&.Mui-checked .MuiSvgIcon-root": {
+                          fill: "#30422E",
+                        },
+                      }}
+                    />
+                  }
+                  label={
+                    <Typography
+                      sx={{
+                        fontSize: { sm: "20px", xs: "16px" },
+                        fontWeight: 400,
+                        color: "rgba(0, 0, 0, 0.6)",
+                      }}
+                    >
+                      Once in a day
+                    </Typography>
+                  }
+                />
+                <FormControlLabel
+                  value="TWODAYS"
+                  checked={questionFrequency === "TWODAYS"}
+                  control={
+                    <Radio
+                      sx={{
+                        "&.Mui-checked .MuiSvgIcon-root": {
+                          fill: "#30422E",
+                        },
+                      }}
+                    />
+                  }
+                  label={
+                    <Typography
+                      sx={{
+                        fontSize: { sm: "20px", xs: "16px" },
+                        fontWeight: 400,
+                        color: "rgba(0, 0, 0, 0.6)",
+                      }}
+                    >
+                      Every other day
+                    </Typography>
+                  }
+                />
+                <FormControlLabel
+                  value="FIVEDAYS"
+                  checked={questionFrequency === "FIVEDAYS"}
+                  control={
+                    <Radio
+                      sx={{
+                        "&.Mui-checked .MuiSvgIcon-root": {
+                          fill: "#30422E",
+                        },
+                      }}
+                    />
+                  }
+                  label={
+                    <Typography
+                      sx={{
+                        fontSize: { sm: "20px", xs: "16px" },
+                        fontWeight: 400,
+                        color: "rgba(0, 0, 0, 0.6)",
+                      }}
+                    >
+                      Once in 3 days
+                    </Typography>
+                  }
+                />
+                <FormControlLabel
+                  value="SEVENDAYS"
+                  checked={questionFrequency === "SEVENDAYS"}
+                  control={
+                    <Radio
+                      sx={{
+                        "&.Mui-checked .MuiSvgIcon-root": {
+                          fill: "#30422E",
+                        },
+                      }}
+                    />
+                  }
+                  label={
+                    <Typography
+                      sx={{
+                        fontSize: { sm: "20px", xs: "16px" },
+                        fontWeight: 400,
+                        color: "rgba(0, 0, 0, 0.6)",
+                      }}
+                    >
+                      Once in a week
+                    </Typography>
+                  }
+                />
+              </RadioGroup>
+            </Box>
+            <Box sx={{ mt: { sm: "50px", xs: "25px" }, mb: "35px" }}>
+              <Typography
+                sx={{
+                  fontSize: { sm: "24px", xs: "20px" },
+                  fontWeight: 700,
+                  color: "#30422E", mb: "20px"
+                }}
+              >
+                Which Emails you want to receive
+              </Typography>
+              <Box>
+                <Box sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  bgcolor: "#fff",
+                  width: { sm: "75%", xs: "100%" },
+                  borderRadius: "4px",
+                  p: "10px",
+                  mb: "15px"
+                }}>
+                  <Checkbox
+                    defaultChecked={false}
+                    style={{ color: "#30422E", marginRight: "7px" }}
+                  // checked={}
+                  // onChange={}
+                  />
+                  <Typography sx={{ color: "#30422E", fontSize: { sm: "20px", xs: "16px" } }}>
+                    Chapter completion confirmations
+                  </Typography>
+                </Box>
+                <Box sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  bgcolor: "#fff",
+                  width: { sm: "75%", xs: "100%" },
+                  borderRadius: "4px",
+                  p: "10px",
+                  mb: "15px"
+                }}>
+                  <Checkbox
+                    defaultChecked={false}
+                    style={{ color: "#30422E", marginRight: "7px" }}
+                  // checked={}
+                  // onChange={}
+                  />
+                  <Typography sx={{ color: "#30422E", fontSize: { sm: "20px", xs: "16px" } }}>
+                    Milestone achievements
+                  </Typography>
+                </Box>
+                <Box sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  bgcolor: "#fff",
+                  width: { sm: "75%", xs: "100%" },
+                  borderRadius: "4px",
+                  p: "10px",
+                  mb: "15px"
+                }}>
+                  <Checkbox
+                    defaultChecked={false}
+                    style={{ color: "#30422E", marginRight: "7px" }}
+                  // checked={}
+                  // onChange={}
+                  />
+                  <Typography sx={{ color: "#30422E", fontSize: { sm: "20px", xs: "16px" } }}>
+                    LifeScript newsletter
+                  </Typography>
+                </Box>
+                <Box sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  bgcolor: "#fff",
+                  width: { sm: "75%", xs: "100%" },
+                  borderRadius: "4px",
+                  p: "10px",
+                  mb: "15px"
+                }}>
+                  <Checkbox
+                    defaultChecked={false}
+                    style={{ color: "#30422E", marginRight: "7px" }}
+                  // checked={}
+                  // onChange={}
+                  />
+                  <Typography sx={{ color: "#30422E", fontSize: { sm: "20px", xs: "16px" } }}>
+                    Occasional promotions
+                  </Typography>
+                </Box>
+              </Box>
+            </Box>
+            <GlobelBtn
+              borderRadius="4px"
+              bgColor="#E1683B"
+              color="white"
+              btnText="Next"
+              onClick={handleSubmit}
+            />
           </Box>
+
+
         </Box>
       </Layout>
 
