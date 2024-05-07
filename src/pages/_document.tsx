24c7fad8c8d8c8bd6fac445468cd74601bd29fce
tayyab-103 <tayyabhameed103@gmail.com>
2024-05-07 15:16:50 +0500

    Add new Script Microsoft Clarity

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,7 +13,7 @@ export default function Document() {
         />
 
         {/* Clarity Analytics Code */}
-        <Script
+        {/* <Script
           id="clarity"
           type="text/javascript"
           dangerouslySetInnerHTML={{
@@ -25,7 +25,17 @@ export default function Document() {
               })(window, document, "clarity", "script", "lpnt5cc9c6");
             `,
           }}
-        />
+        /> */}
+
+        <script type="text/javascript">
+          {`
+          (function(c,l,a,r,i,t,y){
+            c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "lpnt5cc9c6");
+          `}
+        </script>
 
 
 
