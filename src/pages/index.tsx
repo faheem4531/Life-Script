01bd5fb3265c859dbd194d56f73c3f04ce89d7d4
tayyab-103 <tayyabhameed103@gmail.com>
2024-05-07 14:59:35 +0500

    add script in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,6 +26,40 @@ export default function Home() {
         <meta property="og:image" content="https://github.com/Tayyab-103/JavaScript-103/assets/109760448/26d04d82-e102-4c24-961b-9954303bd1b5" />
       </Head> */}
 
+      
+        {/* Clarity Analytics Code */}
+        <Script
+          id="clarity"
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/lpnt5cc9c6";
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "lpnt5cc9c6");
+            `,
+          }}
+        />
+
+
+
+        {/* Google Analytics Code */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-RGM8D41H5K"
+        />
+
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-RGM8D41H5K');
+          `}
+        </Script>
+
       {/* <FeaturesPage /> */}
       <HomePage />
      
