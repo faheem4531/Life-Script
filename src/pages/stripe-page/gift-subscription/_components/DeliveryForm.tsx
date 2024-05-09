8d771b71d57cc7bd61297bd0c778c079cd903a8a
tayyab-103 <tayyabhameed103@gmail.com>
2024-05-09 12:16:54 +0500

    Set validation in delivery form

--- src/pages/stripe-page/gift-subscription/_components/DeliveryForm.tsx
+++ src/pages/stripe-page/gift-subscription/_components/DeliveryForm.tsx
@@ -146,10 +146,15 @@ const DeliveryForm = ({ onClick, selectedTab, inAppGiftFlow, newData, setGiftToU
     },
     validationSchema: Yup.object({
       email: Yup.string().matches(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/, "Invalid email format").required(t("signup-page.emailRequired")),
-      name: Yup.string().matches(/^[A-Za-z\s]+$/, "Name must contain only letters").required("Name is required"),
+      name: Yup.string()
+        .matches(/^[A-Za-z\s]+$/, "Name must contain only letters")
+        .min(3, "Name must be at least 3 characters")
+        .required("Name is required"),
       sendGiftDate: Yup.date().min(new Date(), "Please select a future date").required("Please select a date"),
       giftFrom: Yup.string().matches(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/, "Invalid email format").required("Sender's email is required"),
-      giftFromName: Yup.string().matches(/^[A-Za-z\s]+$/, "Sender's name must contain only letters").required("Sender's name is required"),
+      giftFromName: Yup.string().matches(/^[A-Za-z\s]+$/, "Sender's name must contain only letters") 
+      .min(3, "Name must be at least 3 characters")
+      .required("Name is required"),
       giftMessage: Yup.string().required("Gift message is required"),
       // Add more validation rules as needed
     }),
