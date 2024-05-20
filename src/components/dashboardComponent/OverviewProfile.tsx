8beee3ccc256059cd8622a6363c41a510e54772e
faheem4531 <faheem4531@gmail.com>
2024-05-20 15:44:30 +0500

    badages spacing issue

--- src/components/dashboardComponent/OverviewProfile.tsx
+++ src/components/dashboardComponent/OverviewProfile.tsx
@@ -5,50 +5,51 @@ import { useTranslation } from "react-i18next";
 import styles from "./Custom.module.css";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { getChapters, getHours, selectAllChapters } from "@/store/slices/chatSlice";
+import {
+  getChapters,
+  getHours,
+  selectAllChapters,
+} from "@/store/slices/chatSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserProfile, selectUser } from "@/store/slices/authSlice";
 
 import Grey from "@/_assets/svg/Silver Token.svg";
-import Word8 from "@/_assets/png/achievements/word-8.png"
-import Word7 from "@/_assets/png/achievements/word-7.png"
-import Word6 from "@/_assets/png/achievements/word-6.png"
-import Word5 from "@/_assets/png/achievements/word-5.png"
-import Word4 from "@/_assets/png/achievements/word-4.png"
-import Word3 from "@/_assets/png/achievements/word-3.png"
-import Word2 from "@/_assets/png/achievements/word-2.png"
-import Word1 from "@/_assets/png/achievements/word-1.png"
-import Word0 from "@/_assets/png/achievements/word-0.png"
-import Chapter8 from "@/_assets/png/achievements/chapter-8.png"
-import Chapter7 from "@/_assets/png/achievements/chapter-7.png"
-import Chapter6 from "@/_assets/png/achievements/chapter-6.png"
-import Chapter5 from "@/_assets/png/achievements/chapter-5.png"
-import Chapter4 from "@/_assets/png/achievements/chapter-4.png"
-import Chapter3 from "@/_assets/png/achievements/chapter-3.png"
-import Chapter2 from "@/_assets/png/achievements/chapter-2.png"
-import Chapter1 from "@/_assets/png/achievements/chapter-1.png"
-import Chapter0 from "@/_assets/png/achievements/chapter-0.png"
-import Query8 from "@/_assets/png/achievements/query-8.png"
-import Query7 from "@/_assets/png/achievements/query-7.png"
-import Query6 from "@/_assets/png/achievements/query-6.png"
-import Query5 from "@/_assets/png/achievements/query-5.png"
-import Query4 from "@/_assets/png/achievements/query-4.png"
-import Query3 from "@/_assets/png/achievements/query-3.png"
-import Query2 from "@/_assets/png/achievements/query-2.png"
-import Query1 from "@/_assets/png/achievements/query-1.png"
-import Query0 from "@/_assets/png/achievements/query-0.png"
-import Time8 from "@/_assets/png/achievements/time-8.png"
-import Time7 from "@/_assets/png/achievements/time-7.png"
-import Time6 from "@/_assets/png/achievements/time-6.png"
-import Time5 from "@/_assets/png/achievements/time-5.png"
-import Time4 from "@/_assets/png/achievements/time-4.png"
-import Time3 from "@/_assets/png/achievements/time-3.png"
-import Time2 from "@/_assets/png/achievements/time-2.png"
-import Time1 from "@/_assets/png/achievements/time-1.png"
-import Time0 from "@/_assets/png/achievements/time-0.png"
-
-
-
+import Word8 from "@/_assets/png/achievements/word-8.png";
+import Word7 from "@/_assets/png/achievements/word-7.png";
+import Word6 from "@/_assets/png/achievements/word-6.png";
+import Word5 from "@/_assets/png/achievements/word-5.png";
+import Word4 from "@/_assets/png/achievements/word-4.png";
+import Word3 from "@/_assets/png/achievements/word-3.png";
+import Word2 from "@/_assets/png/achievements/word-2.png";
+import Word1 from "@/_assets/png/achievements/word-1.png";
+import Word0 from "@/_assets/png/achievements/word-0.png";
+import Chapter8 from "@/_assets/png/achievements/chapter-8.png";
+import Chapter7 from "@/_assets/png/achievements/chapter-7.png";
+import Chapter6 from "@/_assets/png/achievements/chapter-6.png";
+import Chapter5 from "@/_assets/png/achievements/chapter-5.png";
+import Chapter4 from "@/_assets/png/achievements/chapter-4.png";
+import Chapter3 from "@/_assets/png/achievements/chapter-3.png";
+import Chapter2 from "@/_assets/png/achievements/chapter-2.png";
+import Chapter1 from "@/_assets/png/achievements/chapter-1.png";
+import Chapter0 from "@/_assets/png/achievements/chapter-0.png";
+import Query8 from "@/_assets/png/achievements/query-8.png";
+import Query7 from "@/_assets/png/achievements/query-7.png";
+import Query6 from "@/_assets/png/achievements/query-6.png";
+import Query5 from "@/_assets/png/achievements/query-5.png";
+import Query4 from "@/_assets/png/achievements/query-4.png";
+import Query3 from "@/_assets/png/achievements/query-3.png";
+import Query2 from "@/_assets/png/achievements/query-2.png";
+import Query1 from "@/_assets/png/achievements/query-1.png";
+import Query0 from "@/_assets/png/achievements/query-0.png";
+import Time8 from "@/_assets/png/achievements/time-8.png";
+import Time7 from "@/_assets/png/achievements/time-7.png";
+import Time6 from "@/_assets/png/achievements/time-6.png";
+import Time5 from "@/_assets/png/achievements/time-5.png";
+import Time4 from "@/_assets/png/achievements/time-4.png";
+import Time3 from "@/_assets/png/achievements/time-3.png";
+import Time2 from "@/_assets/png/achievements/time-2.png";
+import Time1 from "@/_assets/png/achievements/time-1.png";
+import Time0 from "@/_assets/png/achievements/time-0.png";
 
 const Profile = ({ data }) => {
   const { t } = useTranslation();
@@ -60,7 +61,6 @@ const Profile = ({ data }) => {
   const [progressChapters, setProgressChapters] = useState([]);
   const [userName, setUserName] = useState("");
   const [hoursCount, setHoursCount] = useState("");
-
 
   function calculateCompletionPercentage(array) {
     if (array?.length === 0) {
@@ -90,11 +90,9 @@ const Profile = ({ data }) => {
     }
   }
 
-
-
   useEffect(() => {
     const name = localStorage.getItem("username");
-    const firstName = name ? name.split(' ')[0] : ''; // Get the part before the first space
+    const firstName = name ? name.split(" ")[0] : ""; // Get the part before the first space
     setUserName(firstName);
     dispatch(getChapters());
     dispatch(getUserProfile());
@@ -119,44 +117,42 @@ const Profile = ({ data }) => {
     }
   }, [userData]);
 
-
-
   const wordCount = data?.words;
-  const questionsCount = data?.questions
-  const chaptersCount = data?.chapters
-  const timeCount = hoursCount
-
+  const questionsCount = data?.questions;
+  const chaptersCount = data?.chapters;
+  const timeCount = hoursCount;
 
   const getWordsBadgeAndTooltip = (wordCount) => {
     let WordbadgeImage, WordtooltipText;
 
     if (wordCount < 1000) {
       WordbadgeImage = Word0;
-      WordtooltipText = 'Word Wizard will be opened after writing at least 1000 words';
+      WordtooltipText =
+        "Word Wizard will be opened after writing at least 1000 words";
     } else if (wordCount >= 1000 && wordCount < 3000) {
       WordbadgeImage = Word1;
-      WordtooltipText = '  1000 words written';
+      WordtooltipText = "  1000 words written";
     } else if (wordCount >= 3000 && wordCount < 5000) {
       WordbadgeImage = Word2;
-      WordtooltipText = '  3000 words written';
+      WordtooltipText = "  3000 words written";
     } else if (wordCount >= 5000 && wordCount < 10000) {
       WordbadgeImage = Word3;
-      WordtooltipText = '  5000 words written';
+      WordtooltipText = "  5000 words written";
     } else if (wordCount >= 10000 && wordCount < 20000) {
       WordbadgeImage = Word4;
-      WordtooltipText = '  10000 words written';
+      WordtooltipText = "  10000 words written";
     } else if (wordCount >= 20000 && wordCount < 30000) {
       WordbadgeImage = Word5;
-      WordtooltipText = '  20000 words written';
+      WordtooltipText = "  20000 words written";
     } else if (wordCount >= 30000 && wordCount < 40000) {
       WordbadgeImage = Word6;
-      WordtooltipText = '  30000 words written';
+      WordtooltipText = "  30000 words written";
     } else if (wordCount >= 40000 && wordCount < 50000) {
       WordbadgeImage = Word7;
-      WordtooltipText = '  40000 words written';
+      WordtooltipText = "  40000 words written";
     } else if (wordCount >= 50000) {
       WordbadgeImage = Word8;
-      WordtooltipText = 'Congratulations!   50000 words written';
+      WordtooltipText = "Congratulations!   50000 words written";
     }
 
     return { WordbadgeImage, WordtooltipText };
@@ -167,31 +163,32 @@ const Profile = ({ data }) => {
 
     if (questionsCount < 5) {
       QsbadgeImage = Query0;
-      QstooltipText = 'Query Master will be opened after writing at least 5 Questions';
+      QstooltipText =
+        "Query Master will be opened after writing at least 5 Questions";
     } else if (questionsCount >= 5 && questionsCount < 10) {
       QsbadgeImage = Query1;
-      QstooltipText = '  5 questions answered';
+      QstooltipText = "  5 questions answered";
     } else if (questionsCount >= 10 && questionsCount < 15) {
       QsbadgeImage = Query2;
-      QstooltipText = '  10 questions answered';
+      QstooltipText = "  10 questions answered";
     } else if (questionsCount >= 15 && questionsCount < 20) {
       QsbadgeImage = Query3;
-      QstooltipText = '  15 questions answered';
+      QstooltipText = "  15 questions answered";
     } else if (questionsCount >= 20 && questionsCount < 30) {
       QsbadgeImage = Query4;
-      QstooltipText = '  20 questions answered';
+      QstooltipText = "  20 questions answered";
     } else if (questionsCount >= 30 && questionsCount < 50) {
       QsbadgeImage = Query5;
-      QstooltipText = '  30 questions answered';
+      QstooltipText = "  30 questions answered";
     } else if (questionsCount >= 50 && questionsCount < 70) {
       QsbadgeImage = Query6;
-      QstooltipText = '  50 questions answered';
+      QstooltipText = "  50 questions answered";
     } else if (questionsCount >= 70 && questionsCount < 100) {
       QsbadgeImage = Query7;
-      QstooltipText = '  70 questions answered';
+      QstooltipText = "  70 questions answered";
     } else if (questionsCount >= 100) {
       QsbadgeImage = Query8;
-      QstooltipText = ' 100 questions answered';
+      QstooltipText = " 100 questions answered";
     }
 
     return { QsbadgeImage, QstooltipText };
@@ -202,31 +199,32 @@ const Profile = ({ data }) => {
 
     if (chaptersCount == 0) {
       ChapterbadgeImage = Chapter0;
-      ChaptertooltipText = 'Chapter Champ will be opened after completing at least 1 chapter';
+      ChaptertooltipText =
+        "Chapter Champ will be opened after completing at least 1 chapter";
     } else if (chaptersCount >= 1 && chaptersCount < 3) {
       ChapterbadgeImage = Chapter1;
-      ChaptertooltipText = '  1 chapter completed';
+      ChaptertooltipText = "  1 chapter completed";
     } else if (chaptersCount >= 3 && chaptersCount < 5) {
       ChapterbadgeImage = Chapter2;
-      ChaptertooltipText = '  3 chapters completed';
+      ChaptertooltipText = "  3 chapters completed";
     } else if (chaptersCount >= 5 && chaptersCount < 7) {
       ChapterbadgeImage = Chapter3;
-      ChaptertooltipText = '  5 chapters completed';
+      ChaptertooltipText = "  5 chapters completed";
     } else if (chaptersCount >= 7 && chaptersCount < 10) {
       ChapterbadgeImage = Chapter4;
-      ChaptertooltipText = '  7 chapters completed';
+      ChaptertooltipText = "  7 chapters completed";
     } else if (chaptersCount >= 10 && chaptersCount < 15) {
       ChapterbadgeImage = Chapter5;
-      ChaptertooltipText = '  10 chapters completed';
+      ChaptertooltipText = "  10 chapters completed";
     } else if (chaptersCount >= 15 && chaptersCount < 20) {
       ChapterbadgeImage = Chapter6;
-      ChaptertooltipText = '  15 chapters completed';
+      ChaptertooltipText = "  15 chapters completed";
     } else if (chaptersCount >= 20 && chaptersCount < 25) {
       ChapterbadgeImage = Chapter7;
-      ChaptertooltipText = '  20 chapters completed';
+      ChaptertooltipText = "  20 chapters completed";
     } else if (chaptersCount >= 25) {
       ChapterbadgeImage = Chapter8;
-      ChaptertooltipText = ' 25 chapters completed';
+      ChaptertooltipText = " 25 chapters completed";
     }
 
     return { ChapterbadgeImage, ChaptertooltipText };
@@ -235,44 +233,47 @@ const Profile = ({ data }) => {
   const getTimeBadgeAndTooltip = (timeCount) => {
     let timebadgeImage, timetooltipText;
 
-    if (timeCount.includes('m')) {
+    if (timeCount.includes("m")) {
       timebadgeImage = Time0;
-      timetooltipText = 'Time Tracker will be opened after writing for at least 1 hour';
-    } else if (timeCount.includes('h')) {
+      timetooltipText =
+        "Time Tracker will be opened after writing for at least 1 hour";
+    } else if (timeCount.includes("h")) {
       const hours = parseInt(timeCount);
       if (hours >= 1 && hours <= 4) {
         timebadgeImage = Time1;
-        timetooltipText = '1 hour of writing';
+        timetooltipText = "1 hour of writing";
       } else if (hours >= 5 && hours <= 9) {
         timebadgeImage = Time2;
-        timetooltipText = '5 hours of writing';
+        timetooltipText = "5 hours of writing";
       } else if (hours >= 10 && hours <= 19) {
         timebadgeImage = Time3;
-        timetooltipText = '10 hours of writing';
+        timetooltipText = "10 hours of writing";
       } else if (hours >= 20 && hours <= 29) {
         timebadgeImage = Time4;
-        timetooltipText = '20 hours of writing';
+        timetooltipText = "20 hours of writing";
       } else if (hours >= 30 && hours <= 49) {
         timebadgeImage = Time5;
-        timetooltipText = '30 hours of writing';
+        timetooltipText = "30 hours of writing";
       } else if (hours >= 50 && hours <= 69) {
         timebadgeImage = Time6;
-        timetooltipText = '50 hours of writing';
+        timetooltipText = "50 hours of writing";
       } else if (hours >= 70 && hours <= 99) {
         timebadgeImage = Time7;
-        timetooltipText = '70 hours of writing';
+        timetooltipText = "70 hours of writing";
       } else if (hours >= 100) {
         timebadgeImage = Time8;
-        timetooltipText = '100 hours of writing';
+        timetooltipText = "100 hours of writing";
       }
     }
 
     return { timebadgeImage, timetooltipText };
   };
 
-  const { WordbadgeImage, WordtooltipText } = getWordsBadgeAndTooltip(wordCount);
+  const { WordbadgeImage, WordtooltipText } =
+    getWordsBadgeAndTooltip(wordCount);
   const { QsbadgeImage, QstooltipText } = getQsBadgeAndTooltip(questionsCount);
-  const { ChapterbadgeImage, ChaptertooltipText } = getChaptersBadgeAndTooltip(chaptersCount);
+  const { ChapterbadgeImage, ChaptertooltipText } =
+    getChaptersBadgeAndTooltip(chaptersCount);
   const { timebadgeImage, timetooltipText } = getTimeBadgeAndTooltip(timeCount);
 
   return (
@@ -299,14 +300,13 @@ const Profile = ({ data }) => {
           {t("overView.achivement")}
         </Typography>
 
-
         <Box
           sx={{
             display: "flex",
             justifyContent: "center",
             margin: "30px 0 40px",
-            columnGap: { xl: "20px", lg: "15px" },
-            rowGap: "15px"
+            columnGap: { xl: "20px", lg: "15px", sm: "20px", xs: "20px" },
+            rowGap: "15px",
           }}
         >
           <Box>
@@ -339,8 +339,6 @@ const Profile = ({ data }) => {
                   className={styles.profileAchivements}
                 />
               </Tooltip>
-
-
             </Box>
             <Box sx={{ cursor: "pointer", mt: "20px" }}>
               <Tooltip title={timetooltipText}>
@@ -353,8 +351,6 @@ const Profile = ({ data }) => {
             </Box>
           </Box>
         </Box>
-
-
 
         <Typography sx={{ fontSize: "14px", fontWeight: 700 }}>
           {t("overView.RecentCh")}
@@ -421,7 +417,11 @@ export const RecentChapters = ({ title, percentage, id }) => {
 function CircularProgressWithLabel(props) {
   return (
     <Box sx={{ position: "relative", display: "inline-flex", width: "24px" }}>
-      <CircularProgress sx={{ color: "#E1683B" }} variant="determinate" {...props} />
+      <CircularProgress
+        sx={{ color: "#E1683B" }}
+        variant="determinate"
+        {...props}
+      />
       <Box
         sx={{
           top: 2,
