5965a37fe0526df614477f2689cfe7c819ea3ada
tayyab-103 <tayyabhameed103@gmail.com>
2024-05-27 12:17:21 +0500

    removed facebook permission script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -258,17 +258,6 @@ export default function Document() {
 
       </Head>
       <body style={{ margin: 0 }}>
-      <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                const warningTitleCSS = 'color:red; font-size:60px; font-weight: bold; -webkit-text-stroke: 1px black;';
-                const warningDescCSS = 'font-size: 18px;';
-                console.log('%cStop!', warningTitleCSS);
-                console.log("%cThis is a browser feature intended for developers. If someone told you to copy and paste something here to enable a Facebook feature or \\"hack\\" someone's account, it is a scam and will give them access to your Facebook account.", warningDescCSS);
-                console.log('%cSee https://www.facebook.com/selfxss for more information.', warningDescCSS);
-              `,
-            }}
-          />
         <Main />
         {/* Paste the Plerdy tracking script here */}
         <Script
