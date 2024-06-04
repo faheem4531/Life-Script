59561664fc81480c5e906ac16b5dc29e213d7365
faheem4531 <faheem4531@gmail.com>
2024-06-05 02:35:06 +0500

    fix fuild serror

--- src/utils/constants/constants.ts
+++ src/utils/constants/constants.ts
@@ -2,16 +2,16 @@ import grandmaBookImage from "../../../public/grandmaBookImage.svg";
 import premiumBookImage from "../../../public/premiumBookImage.svg";
 import standardBookImage from "../../../public/standardBookImage.svg";
 
-import { BasicArray, PremiumArray, StandardArray } from "../stripeFlowObjects";
+import { createBasicArray, createPremiumArray, createStandardArray } from "../stripeFlowObjects";
 
 export const imageMapping = {
-    Basic: grandmaBookImage,
-    Standard: standardBookImage,
-    Premium: premiumBookImage,
-  };
+  Basic: grandmaBookImage,
+  Standard: standardBookImage,
+  Premium: premiumBookImage,
+};
 
-  export const categoryArrayMapping = {
-    Basic: BasicArray,
-    Standard: StandardArray,
-    Premium: PremiumArray,
-  };
+export const categoryArrayMapping = {
+  Basic: createBasicArray,
+  Standard: createStandardArray,
+  Premium: createPremiumArray,
+};
