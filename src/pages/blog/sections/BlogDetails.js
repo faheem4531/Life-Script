cc29121c85b095421ce3f6ebfe09b80179447515
awais-tdc <akhtar.awais@thedevcorporate.com>
2024-07-24 20:42:48 +0500

    starpi plugins install  & customize the blog

--- src/pages/blog/sections/BlogDetails.js
+++ src/pages/blog/sections/BlogDetails.js
@@ -1,35 +1,24 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import styles from "./BlogSection.module.css";
-import Content from "@/__webComponents/headings/Content";
-import markdownit from "markdown-it";
+import Image from "next/image";
+import BgImage from "@/__webAssets/pngs/bg-blog.png";
 
-
-const BlogDetails = ({details }) => {
-
-  const md = new markdownit();
-  const htmlContent = md.render(details);
-  
+const BlogDetails = ({ details, title, date, image }) => {
+  const htmlContent = details;
 
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", rowGap: "20px" }}>
-      <section
-      dangerouslySetInnerHTML={{__html:htmlContent}}
-      className={styles.content}
-      >
-        {/* {htmlContent} */}
-      </section>
-      {/* <Content
-          width="100%"
-          subFonts="20px"
-          subWidth="100%"
-          mblcolor="#E1683B"
-          heading=""
-          subHeading={details} 
-        /> */}
-
-    </Box>
+    <>
+      <Image src={BgImage} alt="Background image" className={styles.bgImage} />
+      <Box sx={{ width: "100%", margin: "auto" , marginTop : 3}}>
+        <Box className={styles.blogImgContainer}>
+            <section
+          dangerouslySetInnerHTML={{ __html: htmlContent }}
+          className={styles.content}
+        />
+        </Box>
+      </Box>
+    </>
   );
 };
-
 
 export default BlogDetails;
