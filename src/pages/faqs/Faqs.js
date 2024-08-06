73f4b2fbfa78aae531486cef65f3e48183ac4266
faheem4531 <faheem4531@gmail.com>
2024-08-06 16:55:25 +0500

    Avenir fonts path change

--- src/pages/faqs/Faqs.js
+++ src/pages/faqs/Faqs.js
@@ -1,10 +1,10 @@
-'use client'
+"use client";
 
 import { Box, Typography } from "@mui/material";
-import styles from "./Faq.module.css"
 import Image from "next/image";
+import styles from "./Faq.module.css";
 
-import Points from "@/__webAssets/svgs/lifescript-frequently-asked-questions-element.svg"
+import Points from "@/__webAssets/svgs/lifescript-frequently-asked-questions-element.svg";
 import { useTranslation } from "react-i18next";
 const Faqs = () => {
   const { t } = useTranslation();
@@ -73,28 +73,53 @@ const Faqs = () => {
       question: t("faqsSections.question16.question"),
       answer: t("faqsSections.question16.answer"),
     },
-  ]
+  ];
 
   return (
-    <Box sx={{
-      padding: { lg: "200px 260px 150px 150px", md: "150px 70px 150px 100px", sm: "100px 50px", xs: "80px 16px 150px" }
-    }}
+    <Box
+      sx={{
+        padding: {
+          lg: "200px 260px 150px 150px",
+          md: "150px 70px 150px 100px",
+          sm: "100px 50px",
+          xs: "80px 16px 150px",
+        },
+      }}
       className={styles.hurtBg}
     >
-
-      <Box sx={{
-        borderBottom: "1px solid #E1683B",
-        paddingBottom: { md: "35px", sx: "25px", xs: "15px" },
-        paddingLeft: { sm: "0", xs: "15px" }
-      }}>
-        <Typography sx={{ fontSize: { md: "60px", sm: "50px", xs: "32px" }, fontWeight: 500, fontFamily: "Besley !important" }}>
+      <Box
+        sx={{
+          borderBottom: "1px solid #E1683B",
+          paddingBottom: { md: "35px", sx: "25px", xs: "15px" },
+          paddingLeft: { sm: "0", xs: "15px" },
+        }}
+      >
+        <Typography
+          sx={{
+            fontSize: { md: "60px", sm: "50px", xs: "32px" },
+            fontWeight: 500,
+            fontFamily: "Besley !important",
+          }}
+        >
           <h1 className={styles.pureHeadings}>{t("faqsSections.title")}</h1>
         </Typography>
         {/* <Typography sx={{ fontSize: "16px", fontWeight: 500 }}> */}
-        <h2 className={styles.pureHeadings} >{t("faqsSections.description")}</h2>
+        <Typography
+          variant="h2"
+          fontFamily="Avenir"
+          className={styles.pureHeadings}
+        >
+          {t("faqsSections.description")}
+        </Typography>
         {/* </Typography> */}
       </Box>
-      <Box sx={{ marginTop: { md: "120px", sx: "80px", xs: "50px" }, display: "flex", position: "relative" }}>
+      <Box
+        sx={{
+          marginTop: { md: "120px", sx: "80px", xs: "50px" },
+          display: "flex",
+          position: "relative",
+        }}
+      >
         <Image
           src={Points}
           alt="A graphic element in the FAQ section that presents beautifully the answers to most common questions - LifeScript"
@@ -102,47 +127,68 @@ const Faqs = () => {
           className={styles.qsPoints}
         />
 
-        <Box >
-          {QAs.map((item, index) => <QuestionNo
-            key={index}
-            qs={item.question}
-            ans={item.answer}
-            index={index}
-          />)}
+        <Box>
+          {QAs.map((item, index) => (
+            <QuestionNo
+              key={index}
+              qs={item.question}
+              ans={item.answer}
+              index={index}
+            />
+          ))}
         </Box>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
 export default Faqs;
 
 function QuestionNo({ qs, ans, index }) {
   return (
-    <Box sx={{
-      margin: { md: " 0 0 70px 60px", sx: "0 0 50px 0", xs: "0 0 25px 0" },
-      maxWidth: "820px",
-      borderBottom: index < 15 && { sm: "0", xs: "1px solid #E1683B" },
-      paddingBottom: index < 15 && { sm: "0", xs: "20px" }
-    }} >
-      <Box sx={{
-        fontWeight: 500,
-        fontFamily: "Besley !important",
-        display: "flex",
-        columnGap: "10px"
-      }}>
-        <Typography sx={{
-          fontSize: { sm: "24px", xs: "20px" }, fontFamily: "Avenir5 !important", width: { sm: "30px", xs: "20px" },
-        }}>
-          {index + 1}{"."}
+    <Box
+      sx={{
+        margin: { md: " 0 0 70px 60px", sx: "0 0 50px 0", xs: "0 0 25px 0" },
+        maxWidth: "820px",
+        borderBottom: index < 15 && { sm: "0", xs: "1px solid #E1683B" },
+        paddingBottom: index < 15 && { sm: "0", xs: "20px" },
+      }}
+    >
+      <Box
+        sx={{
+          fontWeight: 500,
+          fontFamily: "Besley !important",
+          display: "flex",
+          columnGap: "10px",
+        }}
+      >
+        <Typography
+          sx={{
+            fontSize: { sm: "24px", xs: "20px" },
+            fontFamily: "Avenir5 !important",
+            width: { sm: "30px", xs: "20px" },
+          }}
+        >
+          {index + 1}
+          {"."}
         </Typography>
-        <Typography sx={{
-          fontSize: { sm: "24px", xs: "20px" }, fontFamily: "Avenir5 !important"
-        }}>
-          <h3 className={styles.pureHeadings} >{qs}</h3>
+        <Typography
+          sx={{
+            fontSize: { sm: "24px", xs: "20px" },
+            fontFamily: "Avenir5 !important",
+          }}
+        >
+          <h3 className={styles.pureHeadings}>{qs}</h3>
         </Typography>
       </Box>
-      <Typography sx={{ fontSize: "16px", padding: { sm: "20px 0 0 40px", xs: "15px 0 0 30px" } }}>{ans}</Typography>
+      <Typography
+        sx={{
+          fontSize: "16px",
+          padding: { sm: "20px 0 0 40px", xs: "15px 0 0 30px" },
+        }}
+      >
+        {ans}
+      </Typography>
     </Box>
-  )
+  );
 }
