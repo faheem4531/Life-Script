35c9ef4447eb9f853fe53fbdf29cfcfa942b3ad8
faheem4531 <faheem4531@gmail.com>
2024-09-24 03:30:47 +0500

    ty upgrade-standard

---
+++ src/pages/thank-you/upgrade-premium.tsx
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+import Thankyou from "@/components/webComponents/Thankyou";
+
+const UpgradeBasic = () => {
+  const router = useRouter();
+
+  function handleRoutes() {
+    router.push("/dashboard/overview");
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Congratulations on Upgrading</title>
+      </Head>
+      <Thankyou
+        onClick={handleRoutes}
+        head1={"THANK YOU FOR"}
+        head2={"UPGRADING!"}
+        typo1={`You’ve upgraded to the Premium Plan.`}
+        typo2={"A confirmation email will be sent shortly."}
+      />
+    </>
+  )
+}
+
+export default UpgradeBasic
