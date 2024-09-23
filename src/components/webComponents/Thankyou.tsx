33e2e4315f453868eee6753727a341985329e4ad
faheem4531 <faheem4531@gmail.com>
2024-09-23 14:55:36 +0500

    free trail page structure

---
+++ src/components/webComponents/Thankyou.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material"
+
+const Thankyou = () => {
+  return (
+    <Box>
+      helo
+    </Box>
+  )
+}
+
+export default Thankyou
---
+++ src/pages/thank-you/free-trail.tsx
@@ -0,0 +1,11 @@
+import Thankyou from "@/components/webComponents/Thankyou"
+
+const FreeTrail = () => {
+  return (
+    <>
+      <Thankyou />
+    </>
+  )
+}
+
+export default FreeTrail
