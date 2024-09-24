5ab99a692b4e7ce12aacb3853accc0588a84746a
faheem4531 <faheem4531@gmail.com>
2024-09-24 21:10:58 +0500

    buy standard

---
+++ src/pages/thank-you/buy-standard.tsx
@@ -0,0 +1,38 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+import Thankyou from "@/components/webComponents/Thankyou";
+
+const UpgradeBasic = () => {
+  const router = useRouter();
+
+  function handleRoutes() {
+    const auth = router.query.auth || '';
+    
+    if (auth === "auto") {
+      console.log("if 111")
+      const userName = localStorage.getItem("username");
+      router.push(`/getStarted/getTitle?userName=${userName}`);
+    } else {
+      router.push('/_auth/Auth');
+      console.log("if 222")
+    }
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Thank You For Your Gift Purchase</title>
+      </Head>
+      <Thankyou
+        onClick={handleRoutes}
+        head1={"THANK YOU FOR"}
+        head2={"YOUR PURCHASE!"}
+        typo1={"You’ve purchased the Standard Plan."}
+        typo2={"A confirmation email will be sent shortly."}
+      />
+    </>
+  )
+}
+
+export default UpgradeBasic
