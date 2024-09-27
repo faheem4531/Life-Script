070ba882e278a93e9e4cc5d536b3a6af7659eb6a
angel940927 <angelangelov10@gmail.com>
2024-09-27 20:55:06 +0500

    deployment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -31,7 +31,6 @@ export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     const userLoggedIn = localStorage.getItem("token");
 
-
     const publicRoutes = [
       "/pricing",
       "/verify/verificationSent",
